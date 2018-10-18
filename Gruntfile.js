da22116d102d6441a80c2f1a4632f457fc5fa257
Stephane Bisson <sbisson@wikimedia.org>
2018-10-18 15:55:52 -0400

    Initial structure
    Change-Id: I446a4138038645a8f2fe4ec99e52b326df644fc3

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+
+	var conf = grunt.file.readJSON( 'extension.json' );
+	grunt.initConfig( {
+		banana: conf.MessagesDirs,
+		jsonlint: {
+			all: [
+				'**/*.json',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'default', 'test' );
+};
---
+++ includes/Hooks.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace GrowthExperiments;
+
+class Hooks {
+}
