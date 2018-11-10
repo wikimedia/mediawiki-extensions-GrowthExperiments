036e6add0a7a1e4129ff155507a4f07aa5070d16
Umherirrender <umherirrender_de.wp@web.de>
2018-11-10 12:00:07 +0100

    Add grunt-stylelint to lint less files
    Change-Id: Ife2b146d53aeb2f60dfa4537db110981d9646a2a

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,15 @@
 module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	var conf = grunt.file.readJSON( 'extension.json' );
 	grunt.initConfig( {
+		stylelint: {
+			all: [
+				'modules/**/*.less'
+			]
+		},
 		banana: conf.MessagesDirs,
 		jsonlint: {
 			all: [
@@ -14,6 +20,6 @@ module.exports = function ( grunt ) {
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'jsonlint', 'banana', 'stylelint' ] );
 	grunt.registerTask( 'default', 'test' );
 };
