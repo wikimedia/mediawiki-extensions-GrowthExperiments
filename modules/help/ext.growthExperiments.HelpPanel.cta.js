de539dfc0798c915745c42fe095f6b1786e45480
Roan Kattouw <roan.kattouw@gmail.com>
2018-12-07 18:07:20 -0800

    Help panel: display the help panel when VisualEditor is opened
    Add the help panel module as a VE plugin module, so it's loaded whenever
    VE is loaded. In the module, create the button if it isn't already on
    the page.
    
    Bug: T211059
    Change-Id: I905007ef91464f8cb11ee49e1743f8b1c88c2731

--- modules/help/ext.growthExperiments.HelpPanel.cta.js
+++ modules/help/ext.growthExperiments.HelpPanel.cta.js
@@ -1,6 +1,6 @@
 ( function () {
 	$( function () {
-		var helpCtaButton = OO.ui.ButtonWidget.static.infuse( 'mw-ge-help-panel-cta' ),
+		var $buttonToInfuse = $( '#mw-ge-help-panel-cta' ),
 			windowManager = new OO.ui.WindowManager( { modal: OO.ui.isMobile() } ),
 			$overlay = $( '<div>' ).addClass( 'mw-ge-help-panel-widget-overlay' ),
 			/**
@@ -9,12 +9,32 @@
 			helpPanelProcessDialog = new mw.libs.ge.HelpPanelProcessDialog( {
 				size: OO.ui.isMobile() ? 'full' : 'small'
 			} ),
+			helpCtaButton,
 			lifecycle;
+
+		if ( $buttonToInfuse.length ) {
+			helpCtaButton = OO.ui.ButtonWidget.static.infuse( $buttonToInfuse );
+		} else {
+			helpCtaButton = new OO.ui.ButtonWidget( {
+				classes: [ 'mw-ge-help-panel-cta' ],
+				id: 'mw-ge-help-panel-cta',
+				href: mw.util.getUrl( mw.config.get( 'wgGEHelpPanelHelpDeskTitle' ) ),
+				label: mw.msg( 'growthexperiments-help-panel-cta-button-text' ),
+				icon: 'helpNotice',
+				flags: [ 'primary', 'progressive' ]
+			} );
+			$overlay.append( helpCtaButton.$element );
+		}
+
+		mw.hook( 've.activationComplete' ).add( function () {
+			// If helpCtaButton was in the initial HTML, it's inside the mw-content-text div,
+			// which is now hidden. Reattach it to the overlay instead.
+			$overlay.append( helpCtaButton.$element );
+		} );
 
 		$overlay.append( windowManager.$element );
 		if ( !OO.ui.isMobile() ) {
-			$overlay.addClass( 'mw-ge-help-panel-popup' )
-				.append( windowManager.$element );
+			$overlay.addClass( 'mw-ge-help-panel-popup' );
 		}
 		$( 'body' ).append( $overlay );
 		windowManager.addWindows( [ helpPanelProcessDialog ] );
