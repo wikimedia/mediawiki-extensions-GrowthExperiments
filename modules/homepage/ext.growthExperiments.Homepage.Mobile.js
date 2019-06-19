8532dd459bb65d4ca9392592cc86fcda01e06dbf
Kosta Harlan <kharlan@wikimedia.org>
2019-06-19 10:58:47 -0400

    Homepage: Open ask question dialog in route
    Define a route (`#/homepage/{moduleName}/question`) and navigate to it when the
    homepage question asking modules (help, mentor) are clicked. This allows back
    button behavior to correctly return to the previous location for the MF
    overlay. The route is also used on desktop as it simplifies the code, and
    does not otherwise impact the user experience.
    
    A similar implementation should be done for the help panel, but this patch
    restricts the scope of the fix to the homepage modules for now.
    
    Bug: T226041
    Change-Id: I50047fdf15453e638b337a447bad28d2a26cec5c

--- modules/homepage/ext.growthExperiments.Homepage.Mobile.js
+++ modules/homepage/ext.growthExperiments.Homepage.Mobile.js
@@ -28,7 +28,32 @@
 				return new MobileOverlay(
 					$.extend( { moduleName: moduleName }, getModuleData( moduleName ) )
 				).on( 'hide', function () {
-					logger.log( moduleName, 'mobile-overlay', 'close' );
+					// We don't want to log a close event for the overlay when the user has tapped
+					// on the question dialog, which hides the MobileOverlay. To enforce that we
+					// need to check:
+					// - moduleName is not set to e.g. help/question or mentorship/question
+					// - router path is not one of /homepage/help/question or
+					//   /homepage/mentorship/question
+					// Example:
+					// 1. when you click the "Ask the help desk CTA", moduleName is help and
+					//    router.getPath() is /homepage/help/question.
+					// 2. When you close the process dialog, moduleName is  help/question and
+					//    router.getPath() is /homepage/help.
+					// 3. When you close the overlay, moduleName is help and router.getPath()
+					//    is /homepage/help
+					// We can't only check the router path since /homepage/help is the router
+					// path for both  the event we want to log (closing the overlay) and the
+					// event we don't want to log (closing the process dialog).
+					// If we changed the regex in overlayManager.add( /^\/homepage\/(.*)$/ )
+					// so that it only activated on homepage/{moduleName} and not
+					// homepage/{moduleName}/question, the user would briefly see the overlay
+					// vanish and then the mobile summary view (which was behind the overlay)
+					// before the process dialog opened up, which is a poor user experience.
+					// So the convoluted logic and lengthy comment here is a tradeoff for
+					// better user experience.
+					if ( moduleName.indexOf( '/' ) === -1 && !router.getPath().match( /^\/homepage\/.*\/question$/ ) ) {
+						logger.log( moduleName, 'mobile-overlay', 'close' );
+					}
 				} );
 			} );
 
