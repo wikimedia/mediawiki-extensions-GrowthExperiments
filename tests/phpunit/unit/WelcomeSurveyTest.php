2dd0ce393c8e7c663a91296a43f87f796e190a4a
Kosta Harlan <kharlan@wikimedia.org>
2019-07-09 09:27:55 -0400

    Add tests for freetext test feature flag
    Change-Id: Ib823c0485b5216421dc2b43f694b74859a114bd5
    Follows-Up: I0fa5c152fc0303f30312d57e138c7fa9cbd3b981
    Bug: T226039

---
+++ tests/phpunit/unit/WelcomeSurveyTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace GrowthExperiments\Tests;
+
+use Config;
+use GrowthExperiments\WelcomeSurvey;
+use HashConfig;
+use MediaWikiUnitTestCase;
+use RequestContext;
+use User;
+
+/**
+ * @coversDefaultClass \GrowthExperiments\WelcomeSurvey
+ */
+class WelcomeSurveyTest extends MediaWikiUnitTestCase {
+
+	/**
+	 * @covers ::__construct
+	 */
+	public function testConstruct() {
+		$configMock = $this->getMockBuilder( Config::class )
+			->disableOriginalConstructor()
+			->setMethods( [ 'get', 'has' ] )
+			->getMock();
+		$contextMock = $this->getMockBuilder( RequestContext::class )
+			->disableOriginalConstructor()
+			->getMock();
+		$contextMock->expects( $this->once() )
+			->method( 'getConfig' )
+			->willReturn( $configMock );
+		$this->assertInstanceOf( WelcomeSurvey::class, new WelcomeSurvey( $contextMock ) );
+	}
+
+	/**
+	 * @covers ::__construct
+	 * @covers ::getQuestions
+	 */
+	public function testAllowFreetextResponsesTrue() {
+		$welcomeSurvey = $this->getWelcomeSurveyForFreetextTest( true );
+		$questions = $welcomeSurvey->getQuestions( 'exp2_target_specialpage' );
+		$this->assertArrayHasKey( 'reason-other', $questions );
+		$this->assertArrayHasKey( 'topics-other-nojs', $questions );
+	}
+
+	/**
+	 * @covers ::__construct
+	 * @covers ::getQuestions
+	 */
+	public function testAllowFreetextResponsesFalse() {
+		$welcomeSurvey = $this->getWelcomeSurveyForFreetextTest( false );
+		$questions = $welcomeSurvey->getQuestions( 'exp2_target_specialpage' );
+		$this->assertArrayNotHasKey( 'reason-other', $questions );
+		$this->assertArrayNotHasKey( 'topics-other-nojs', $questions );
+	}
+
+	private function getWelcomeSurveyForFreetextTest( $allowFreetext ) {
+		$configMock = new HashConfig( [
+			'WelcomeSurveyAllowFreetextResponses' => $allowFreetext,
+			'WelcomeSurveyExperimentalGroups' => [ 'exp2_target_specialpage' => [
+				"range" => "x",
+				"format" => "specialpage",
+				"questions" => [
+					"reason",
+					"edited",
+					"topics",
+					"topics-other-js",
+					"email",
+					"mentor-info",
+					"mentor"
+				] ] ]
+		] );
+		$contextMock = $this->getMockBuilder( RequestContext::class )
+			->disableOriginalConstructor()
+			->getMock();
+		$contextMock->expects( $this->any() )
+			->method( 'getConfig' )
+			->willReturn( $configMock );
+		$userMock = $this->getMockBuilder( User::class )
+			->disableOriginalConstructor()
+			->getMock();
+		$contextMock->expects( $this->once() )
+			->method( 'getUser' )
+			->willReturn( $userMock );
+		return new WelcomeSurvey( $contextMock );
+	}
+
+}
