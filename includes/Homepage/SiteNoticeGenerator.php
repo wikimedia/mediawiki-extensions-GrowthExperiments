2ef8aa88986377f79978bfc98c02ff60e44c0e0b
Gergő Tisza <tgr.huwiki@gmail.com>
2019-09-16 23:31:37 +0200

    Homepage: add banners for mobile discovery
    Add mobile banners to help new users discover the homepage.
    Since the situations in which to display the banner are the
    exact same as no-JS discovery, that logic is being reused;
    to keep the number of ResourceLoader modules down, the
    existing module is also reused.
    
    Also change SiteNoticeAfter hook handling to make sure later
    hooks cannot add notices. Our notices will only be seen once
    by the user so that's OK.
    
    Bug: T224883
    Change-Id: I56089762c44906b80153be6c97c232e60771854f

--- includes/Homepage/SiteNoticeGenerator.php
+++ includes/Homepage/SiteNoticeGenerator.php
@@ -14,7 +14,8 @@ class SiteNoticeGenerator {
 	 * @param string $name
 	 * @param string &$siteNotice
 	 * @param \Skin $skin
-	 * @param bool &$minervaEnableSiteNotice
+	 * @param bool &$minervaEnableSiteNotice Reference to $wgMinervaEnableSiteNotice
+	 * @return bool|void Hook return value (ie. false to prevent other notices from displaying)
 	 */
 	public static function setNotice( $name, &$siteNotice, \Skin $skin, &$minervaEnableSiteNotice ) {
 		if ( $skin->getTitle()->isSpecial( 'WelcomeSurvey' ) ) {
@@ -24,31 +25,36 @@ class SiteNoticeGenerator {
 		switch ( $name ) {
 			case HomepageHooks::CONFIRMEMAIL_QUERY_PARAM:
 				if ( $skin->getTitle()->isSpecial( 'Homepage' ) ) {
-					self::setConfirmEmailSiteNotice( $siteNotice, $skin, $minervaEnableSiteNotice );
+					return self::setConfirmEmailSiteNotice( $siteNotice, $skin, $minervaEnableSiteNotice );
 				}
 				break;
 			case 'specialwelcomesurvey':
 				if ( $skin->getTitle()->isSpecial( 'Homepage' ) ) {
-					self::setDiscoverySiteNotice( $siteNotice, $skin, $name );
+					return self::setDiscoverySiteNotice( $siteNotice, $skin, $name, $minervaEnableSiteNotice );
 				}
 				break;
 			case 'welcomesurvey-originalcontext':
 				if ( !$skin->getTitle()->isSpecial( 'Homepage' ) ) {
-					self::setDiscoverySiteNotice( $siteNotice, $skin, $name );
+					return self::setDiscoverySiteNotice( $siteNotice, $skin, $name, $minervaEnableSiteNotice );
 				}
 				break;
 			default:
-				self::maybeShowIfUserAbandonedWelcomeSurvey( $siteNotice, $skin );
-				break;
+				return self::maybeShowIfUserAbandonedWelcomeSurvey(
+					$siteNotice,
+					$skin,
+					$minervaEnableSiteNotice );
 		}
 	}
 
-	private static function maybeShowIfUserAbandonedWelcomeSurvey( &$siteNotice, \Skin $skin ) {
+	private static function maybeShowIfUserAbandonedWelcomeSurvey(
+		&$siteNotice, \Skin $skin, &$minervaEnableSiteNotice
+	) {
 		if ( self::isWelcomeSurveyInReferer( $skin ) ) {
-			self::setDiscoverySiteNotice(
+			return self::setDiscoverySiteNotice(
 				$siteNotice,
 				$skin,
-				'welcomesurvey-originalcontext'
+				'welcomesurvey-originalcontext',
+				$minervaEnableSiteNotice
 			);
 		}
 	}
@@ -66,6 +72,7 @@ class SiteNoticeGenerator {
 	 * @param string &$siteNotice
 	 * @param \Skin $skin
 	 * @param bool &$minervaEnableSiteNotice
+	 * @return bool|void Hook return value (ie. false to prevent other notices from displaying)
 	 */
 	private static function setConfirmEmailSiteNotice(
 		&$siteNotice, \Skin $skin, &$minervaEnableSiteNotice
@@ -84,6 +91,29 @@ class SiteNoticeGenerator {
 				$output->msg( 'confirmemail_loggedin' )->text() )
 		);
 		$minervaEnableSiteNotice = true;
+		// Only triggered for a specific source query parameter, which the user should see only
+		// once, so it's OK to suppress all other banners.
+		return false;
+	}
+
+	/**
+	 * @param string &$siteNotice
+	 * @param \Skin $skin
+	 * @param string $contextName
+	 * @param bool $minervaEnableSiteNotice
+	 * @return bool|void Hook return value (ie. false to prevent other notices from displaying)
+	 */
+	private static function setDiscoverySiteNotice(
+		&$siteNotice, \Skin $skin, $contextName, &$minervaEnableSiteNotice
+	) {
+		if ( Util::isMobile( $skin ) ) {
+			self::setMobileDiscoverySiteNotice( $siteNotice, $skin, $contextName, $minervaEnableSiteNotice );
+		} else {
+			self::setDesktopDiscoverySiteNotice( $siteNotice, $skin, $contextName );
+		}
+		// Only triggered for a specific source query parameter, which the user should see only
+		// once, so it's OK to suppress all other banners.
+		return false;
 	}
 
 	/**
@@ -91,15 +121,19 @@ class SiteNoticeGenerator {
 	 * @param \Skin $skin
 	 * @param string $contextName
 	 */
-	private static function setDiscoverySiteNotice(
+	private static function setDesktopDiscoverySiteNotice(
 		&$siteNotice, \Skin $skin, $contextName
 	) {
+		// No-JS banner (hidden from CSS when there's JS support). The JS version is in
+		// ext.growthExperiments.homepage.discovery.tour.js.
+
 		$output = $skin->getOutput();
 		$output->enableOOUI();
 		$output->addModuleStyles( [
 			'oojs-ui.styles.icons-user',
 			'ext.growthExperiments.Homepage.Discovery.styles'
 		] );
+
 		$username = $skin->getUser()->getName();
 		if ( $contextName === 'specialwelcomesurvey' ) {
 			$msgHeaderKey = 'growthexperiments-homepage-discovery-banner-header';
@@ -108,18 +142,63 @@ class SiteNoticeGenerator {
 			$msgHeaderKey = 'growthexperiments-homepage-discovery-thanks-header';
 			$msgBodyKey = 'growthexperiments-homepage-discovery-thanks-text';
 		}
-		$siteNotice = Html::element( 'span', [ 'class' => 'mw-ge-homepage-discovery-house' ] ) .
+		$siteNotice = Html::rawElement( 'div', [ 'class' => 'mw-ge-homepage-discovery-banner-nojs' ],
+			Html::element( 'span', [ 'class' => 'mw-ge-homepage-discovery-house' ] ) .
 			Html::rawElement( 'span', [ 'class' => 'mw-ge-homepage-discovery-text-content' ],
 				Html::element( 'h2', [ 'class' => 'mw-ge-homepage-discovery-nojs-message' ],
-				$output->msg( $msgHeaderKey )->text() ) .
-			Html::rawElement( 'p', [ 'class' => 'mw-ge-homepage-discovery-nojs-banner-text' ],
-				$output->msg( $msgBodyKey )
-					->params( $username )
-					->rawParams( Html::rawElement( 'span', [], new IconWidget( [ 'icon' => 'userAvatar' ] ) ) .
-								 Html::element( 'span', [], $username )
-					)->parse()
+					$output->msg( $msgHeaderKey )->params( $username )->text() ) .
+				Html::rawElement( 'p', [ 'class' => 'mw-ge-homepage-discovery-nojs-banner-text' ],
+					$output->msg( $msgBodyKey )
+						->params( $username )
+						->rawParams( Html::rawElement( 'span', [], new IconWidget( [ 'icon' => 'userAvatar' ] ) ) .
+							Html::element( 'span', [], $username )
+						)->parse()
+				)
 			)
 		);
+	}
+
+	/**
+	 * @param string &$siteNotice
+	 * @param \Skin $skin
+	 * @param string $contextName
+	 * @param bool $minervaEnableSiteNotice
+	 */
+	private static function setMobileDiscoverySiteNotice(
+		&$siteNotice, \Skin $skin, $contextName, &$minervaEnableSiteNotice
+	) {
+		$output = $skin->getOutput();
+		$output->enableOOUI();
+		$output->addModuleStyles( [
+			'oojs-ui.styles.icons-user',
+			'ext.growthExperiments.Homepage.Discovery.styles'
+		] );
+		$output->addModules( 'ext.growthExperiments.Homepage.Discovery.scripts' );
+
+		$username = $skin->getUser()->getName();
+		$location = ( $contextName === 'specialwelcomesurvey' ) ? 'homepage' : 'nonhomepage';
+		$msgHeaderKey = "growthexperiments-homepage-discovery-mobile-$location-banner-header";
+		$msgBodyKey = "growthexperiments-homepage-discovery-mobile-$location-banner-text";
+
+		$siteNotice = Html::rawElement( 'div', [ 'class' => 'mw-ge-homepage-discovery-banner-mobile' ],
+			Html::element( 'div', [ 'class' => 'mw-ge-homepage-discovery-arrow' ] ) .
+			Html::rawElement( 'div', [ 'class' => 'mw-ge-homepage-discovery-message' ],
+				Html::element( 'h2', [],
+					$output->msg( $msgHeaderKey )->params( $username )->text() ) .
+				Html::rawElement( 'p', [],
+					$output->msg( $msgBodyKey )
+						->params( $username )
+						->rawParams( new IconWidget( [ 'icon' => 'userAvatar' ] ) .
+							// add a word joiner to make the icon stick to the name
+							\UtfNormal\Utils::codepointToUtf8( 0x2060 ) .
+							Html::element( 'span', [], $username )
+						)->parse()
+				)
+			) . new IconWidget( [ 'icon' => 'close',
+				'classes' => [ 'mw-ge-homepage-discovery-banner-close' ] ] )
+		);
+
+		$minervaEnableSiteNotice = true;
 	}
 
 }
---
+++ modules/homepage/ext.growthExperiments.Homepage.Discovery.Mobile.js
@@ -0,0 +1,11 @@
+( function () {
+	'use strict';
+	if ( mw.loader.getState( 'mobile.init' ) ) {
+		mw.loader.using( 'mobile.init' ).done( function () {
+			// eslint-disable-next-line no-jquery/no-global-selector
+			$( '.mw-ge-homepage-discovery-banner-close' ).on( 'click', function () {
+				$( this ).closest( '.mw-ge-homepage-discovery-banner-mobile' ).remove();
+			} );
+		} );
+	}
+}() );
