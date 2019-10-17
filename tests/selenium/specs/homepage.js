7e737ca9f1fc9ae398413b007545204b56b51e2b
ephemeralwaves <thinkcolorful@thinkcolorful.org>
2019-10-17 16:38:23 -0400

    Selenium: disable homepage for a new user
    Create a new user, disable homepage on the user preferences page
    and check that Special:Homepage is disabled.
    
    Bug: T235795
    Change-Id: Id68138733b77bf4b05b69cd59fb96a0b2cec217e

--- tests/selenium/specs/homepage.js
+++ tests/selenium/specs/homepage.js
@@ -1,7 +1,9 @@
 var assert = require( 'assert' ),
 	HomepagePage = require( '../pageobjects/homepage.page' ),
 	PreferencesPage = require( '../pageobjects/preferences.page' ),
-	UserLoginPage = require( 'wdio-mediawiki/LoginPage' );
+	UserLoginPage = require( 'wdio-mediawiki/LoginPage' ),
+	Api = require( 'wdio-mediawiki/Api' ),
+	Util = require( 'wdio-mediawiki/Util' );
 
 describe( 'Homepage', function () {
 
@@ -17,6 +19,26 @@ describe( 'Homepage', function () {
 		HomepagePage.open();
 
 		assert( HomepagePage.homepage.isExisting() );
+
+	} );
+
+	it( 'can be disabled for new user', function () {
+
+		var username = Util.getTestString( 'NewUser-' );
+		var password = Util.getTestString();
+		browser.call( function () {
+			return Api.createAccount( username, password );
+		} );
+		UserLoginPage.login( username, password );
+
+		PreferencesPage.open();
+		browser.execute( ( homepage ) => homepage.scrollIntoView(), browser.element( '[name="wpgrowthexperiments-homepage-enable"]' ).value );
+		PreferencesPage.homepage.waitForVisible();
+		PreferencesPage.homepage.click();
+		PreferencesPage.save.click();
+		HomepagePage.open();
+
+		assert.strictEqual( HomepagePage.homepage.isExisting(), false );
 
 	} );
 
