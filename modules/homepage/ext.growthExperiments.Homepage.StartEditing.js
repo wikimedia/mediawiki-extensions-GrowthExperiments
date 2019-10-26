5e98fc06b83e93f45ef0de42852c19d552195d66
Roan Kattouw <roan.kattouw@gmail.com>
2019-10-25 18:13:10 -0700

    Homepage: Refactor StartEditing.js to not use global selectors
    Just like in the JS for the other buttons that launch dialogs (e.g.
    the mentorship module), we have to both look for the button in the
    initial DOM, and register a hook to look in the mobile overlay DOMs as
    they're created.
    
    This fixes the "See suggested edits" button on mobile, which previously
    wasn't opening the dialog.
    
    Change-Id: Iff72d406e23174e485ffc3ebce797a72133f1c66

--- modules/homepage/ext.growthExperiments.Homepage.StartEditing.js
+++ modules/homepage/ext.growthExperiments.Homepage.StartEditing.js
@@ -1,29 +1,44 @@
 ( function () {
-	var ctaButton, windowManager, dialog,
-		StartEditingDialog = require( './ext.growthExperiments.Homepage.StartEditingDialog.js' ),
+	var StartEditingDialog = require( './ext.growthExperiments.Homepage.StartEditingDialog.js' );
+
+	function setupCta( $container ) {
+		var $buttonElement, ctaButton, dialog, windowManager;
+		$buttonElement = $container.find( '#mw-ge-homepage-startediting-cta' );
+		if ( $buttonElement.length === 0 ) {
+			return;
+		}
+
+		ctaButton = OO.ui.ButtonWidget.static.infuse( $buttonElement );
+		dialog = new StartEditingDialog();
+		windowManager = new OO.ui.WindowManager( { modal: true } );
+
 		// eslint-disable-next-line no-jquery/no-global-selector
-		$buttonElement = $( '#mw-ge-homepage-startediting-cta' );
+		$( 'body' ).append( windowManager.$element );
+		windowManager.addWindows( [ dialog ] );
 
-	if ( $buttonElement.length === 0 ) {
-		return;
-	}
-	ctaButton = OO.ui.ButtonWidget.static.infuse( $buttonElement );
-
-	dialog = new StartEditingDialog();
-	windowManager = new OO.ui.WindowManager( { modal: true } );
-	// eslint-disable-next-line no-jquery/no-global-selector
-	$( 'body' ).append( windowManager.$element );
-	windowManager.addWindows( [ dialog ] );
-
-	ctaButton.on( 'click', function () {
-		var lifecycle = windowManager.openWindow( dialog );
-		lifecycle.closing.done( function ( data ) {
-			if ( data && data.action === 'activate' ) {
-				ctaButton.setDisabled( true );
-				window.location.reload();
-			}
+		ctaButton.on( 'click', function () {
+			var lifecycle = windowManager.openWindow( dialog );
+			lifecycle.closing.done( function ( data ) {
+				if ( data && data.action === 'activate' ) {
+					ctaButton.setDisabled( true );
+					window.location.reload();
+				}
+			} );
+			// TODO maybe restructure the page without refreshing?
+			// Would require AJAX for the new module's contents
 		} );
-		// TODO maybe restructure the page without refreshing?
-		// Would require AJAX for the new module's contents
+	}
+
+	// Try setup for desktop mode and server-side-rendered mobile mode
+	// See also the comment in ext.growthExperiments.Homepage.Mentorship.js
+	// eslint-disable-next-line no-jquery/no-global-selector
+	setupCta( $( '.growthexperiments-homepage-container' ) );
+
+	// Try setup for mobile overlay mode
+	mw.hook( 'growthExperiments.mobileHomepageOverlayHtmlLoaded' ).add( function ( moduleName, $content ) {
+		if ( moduleName === 'start' ) {
+			setupCta( $content );
+		}
 	} );
+
 }() );
