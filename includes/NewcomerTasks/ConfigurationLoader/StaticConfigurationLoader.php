dc1be3559083b7f04e40d8b55c93ed9b58833b99
Gergő Tisza <tgr.huwiki@gmail.com>
2020-01-07 14:46:50 -0800

    Newcomer tasks: Expose task type / topic set in API parameter info
    Bug: T240512
    Change-Id: I1ef7393b430e03074af4c96a29a5fd92eb2def91
    (cherry picked from commit 50f89ebe9798300e4b4bded1d78a863a7909d30f)

---
+++ includes/NewcomerTasks/ConfigurationLoader/StaticConfigurationLoader.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace GrowthExperiments\NewcomerTasks\ConfigurationLoader;
+
+use GrowthExperiments\NewcomerTasks\TaskType\TaskType;
+use GrowthExperiments\NewcomerTasks\Topic\Topic;
+use MediaWiki\Linker\LinkTarget;
+use MessageLocalizer;
+use StatusValue;
+
+/**
+ * A minimal ConfigurationLoader for testing and development which returns preconfigured values.
+ */
+class StaticConfigurationLoader implements ConfigurationLoader {
+
+	/** @var TaskType[]|StatusValue */
+	private $taskTypes;
+
+	/** @var Topic[]|StatusValue */
+	private $topics;
+
+	/** @var LinkTarget[]|StatusValue */
+	private $templateBlacklist;
+
+	/**
+	 * @param TaskType[]|StatusValue $taskTypes
+	 * @param Topic[]|StatusValue $topics
+	 * @param LinkTarget[]|StatusValue $templateBlacklist
+	 */
+	public function __construct( $taskTypes, $topics = [], $templateBlacklist = [] ) {
+		$this->taskTypes = $taskTypes;
+		$this->topics = $topics;
+		$this->templateBlacklist = $templateBlacklist;
+	}
+
+	/** @inheritDoc */
+	public function loadTaskTypes() {
+		return $this->taskTypes;
+	}
+
+	/** @inheritDoc */
+	public function loadTopics() {
+		return $this->topics;
+	}
+
+	/** @inheritDoc */
+	public function loadTemplateBlacklist() {
+		return $this->templateBlacklist;
+	}
+
+	/** @inheritDoc */
+	public function setMessageLocalizer( MessageLocalizer $messageLocalizer ): void {
+	}
+
+}
