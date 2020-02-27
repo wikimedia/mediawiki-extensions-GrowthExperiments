ffff286a1ed26445944c75648be4588a7d4f0669
Gergő Tisza <tgr.huwiki@gmail.com>
2020-02-26 20:26:48 -0800

    Avoid extra URL parameter when guidance is disabled
    Follow-up to Ia5adc8d44:
    * rearrange code for passing task type to guidance code, so that
      the task type URL parameter will be removed even when guidance
      is disabled and the relevant JS code does not load. (This
      approach is not very nice, but the URL parameters will probably
      go away anyway once we unify the tracking code for change tags
      and the one for EventLogging.)
    * remove the blue dot when the edit button is clicked (for VE the
      click doesn't result in a page reload and the dot would end up
      looking awkward)
    * simplify the blue dot JS code a little bit
    
    Change-Id: Ie18a90012c2503a5d8f85fe5ab4c82a1585749a4

--- modules/homepage/suggestededits/ext.growthExperiments.SuggestedEdits.Guidance.js
+++ modules/homepage/suggestededits/ext.growthExperiments.SuggestedEdits.Guidance.js
@@ -1,17 +1,10 @@
 ( function () {
 	'use strict';
-	// @see extensions/VisualEditor/modules/ve-mw/preinit/ve.init.mw.DesktopArticleTarget.init.js
-	var Utils = require( '../../utils/ext.growthExperiments.Utils.js' ),
-		/* eslint-disable-next-line no-jquery/no-global-selector */
-		$caEdit = $( 'li#ca-edit, li#page-actions-edit' ),
-		/* eslint-disable-next-line no-jquery/no-global-selector */
-		$caVeEdit = $( 'li#ca-ve-edit' ),
-		$caEditLink = $caEdit.find( 'a' ),
-		$caVeEditLink = $caVeEdit.find( 'a' ),
-		$edit = null,
+	/* eslint-disable-next-line no-jquery/no-global-selector */
+	var $edit = $( 'li#ca-ve-edit, li#ca-edit, li#page-actions-edit' ).first().find( 'a' ),
 		skin = mw.config.get( 'skin' ),
-		url = new mw.Uri( window.location.href ),
-		taskTypeId = url.query.getasktype,
+		// this is set in ext.growthExperiments.ClickId.js
+		taskTypeId = mw.config.get( 'wgGrowthExperimentsTaskTypeUrlParam' ),
 		guidancePrefName = 'growthexperiments-homepage-suggestededits-guidance-blue-dot',
 		guidancePrefValue;
 
@@ -25,24 +18,18 @@
 		guidancePrefValue[ skin ] = {};
 	}
 
-	Utils.removeQueryParam( url, 'getasktype' );
-
 	if ( guidancePrefValue[ skin ][ taskTypeId ] ) {
 		// The user has already seen the blue dot for this task type and skin and
 		// clicked "edit", don't do anything else.
 		return;
 	}
 
-	if ( $caVeEditLink.length ) {
-		$edit = $caVeEditLink;
-	} else if ( $caEditLink.length ) {
-		$edit = $caEditLink;
-	}
-	if ( $edit && $edit.length && taskTypeId ) {
+	if ( $edit.length && taskTypeId ) {
 		$edit.append( $( '<div>' ).addClass( 'mw-pulsating-dot' ) );
 		guidancePrefValue[ skin ][ taskTypeId ] = true;
 		$edit.on( 'click', function () {
 			new mw.Api().saveOption( guidancePrefName, JSON.stringify( guidancePrefValue ) );
+			$( this ).find( '.mw-pulsating-dot' ).remove();
 		} );
 	}
 }() );
