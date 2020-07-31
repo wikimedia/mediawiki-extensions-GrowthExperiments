97b7efa2f21e871b07296e78979227361b457222
Roan Kattouw <roan.kattouw@gmail.com>
2020-07-31 14:56:58 -0700

    WelcomeSurvey: Use autonyms for language question
    For the tags in the tag widget, use autonyms rather than translated
    language names.
    
    This also makes the meta=languageinfo API request obsolete, since we can
    use $.uls.data.getAutonyms() to get all the information we need.
    Removing the API request reduces the time it takes before the lanugage
    question widget loads.
    
    Bug: T232410
    Change-Id: I43101bb597da9aff41bf281861a3c126b1fb45d4
    (cherry picked from commit b93dfc1c9ed1a8bce2946e5afb9554d31d87fbd5)

--- modules/welcomesurvey/ext.growthExperiments.welcomeSurveyLanguage.js
+++ modules/welcomesurvey/ext.growthExperiments.welcomeSurveyLanguage.js
@@ -4,67 +4,40 @@ $( function () {
 		config = require( './config.json' ),
 		shouldUseLanguageInfoOverlay = OO.ui.isMobile() &&
 			mw.mobileFrontend.require( 'mobile.startup' ).languageInfoOverlay,
-		allowedLanguageValuesPromise = new mw.Api().get( {
-			formatversion: 2,
-			meta: 'languageinfo',
-			liprop: 'code|autonym|name'
-		} ),
-		/**
-		 * @type {OO.Router}
-		 */
-		router = require( 'mediawiki.router' );
-	allowedLanguageValuesPromise.then( function ( response ) {
-		return Object.keys( response.query.languageinfo ).map( function ( key ) {
-			return {
-				url: '#',
-				code: response.query.languageinfo[ key ].code,
-				name: response.query.languageinfo[ key ].name
-			};
-		} );
-	} ).then( function ( data ) {
-		var langCodeMap = {};
-		data.forEach( function ( value ) {
-			langCodeMap[ value.code ] = value.name;
-		}, langCodeMap );
-		return {
-			languages: data,
-			langCodeMap: langCodeMap
-		};
-	} ).then( function ( data ) {
-		return new ULSTagMultiselectWidget( {
-			placeholder: mw.message( 'welcomesurvey-question-languages-placeholder' ).text(),
-			inputPosition: 'outline',
-			tagLimit: config.languageMax,
-			allowedValues: Object.keys( data.langCodeMap ),
-			allowArbitrary: false,
-			allowEditTags: false,
-			languages: data.languages,
-			langCodeMap: data.langCodeMap
-		} );
-	} ).then( function ( widgetInstance ) {
-		if ( shouldUseLanguageInfoOverlay ) {
-			widgetInstance.on( 'inputFocus', function () {
-				// FIXME: navigate is deprecated but navigateTo doesn't seem to trigger
-				// the language searcher overlay.
-				router.navigate( '/languages/all' );
-			} );
-		}
-		return widgetInstance;
-	} ).then( function ( widgetInstance ) {
-		mw.hook( 'mobileFrontend.languageSearcher.linkClick' ).add( function ( lang ) {
-			widgetInstance.addLanguageByCode( lang );
-		} );
-		return widgetInstance;
-	} ).done( function ( widgetInstance ) {
-		new OO.ui.FieldLayout( widgetInstance, {
-			align: 'top',
-			classes: [ 'welcomesurvey-languages-uls' ],
-			label: mw.message( 'welcomesurvey-question-languages-label' ).text(),
-			help: mw.message( 'welcomesurvey-question-languages-help' )
-				.params( [ mw.language.convertNumber( config.languageMax ) ] )
-				.text(),
-			helpInline: true
-		} ).$element.insertBefore( '.welcomesurvey-mentor-info' );
+		/** @type {OO.Router} */
+		router = require( 'mediawiki.router' ),
+		langCodeMap = $.uls.data.getAutonyms(),
+		widgetInstance, fieldLayout;
+
+	widgetInstance = new ULSTagMultiselectWidget( {
+		placeholder: mw.message( 'welcomesurvey-question-languages-placeholder' ).text(),
+		inputPosition: 'outline',
+		tagLimit: config.languageMax,
+		allowedValues: Object.keys( langCodeMap ),
+		allowArbitrary: false,
+		allowEditTags: false,
+		langCodeMap: langCodeMap
 	} );
 
+	if ( shouldUseLanguageInfoOverlay ) {
+		widgetInstance.on( 'inputFocus', function () {
+			// FIXME: navigate is deprecated but navigateTo doesn't seem to trigger
+			// the language searcher overlay.
+			router.navigate( '/languages/all' );
+		} );
+	}
+	mw.hook( 'mobileFrontend.languageSearcher.linkClick' ).add( function ( lang ) {
+		widgetInstance.addLanguageByCode( lang );
+	} );
+
+	fieldLayout = new OO.ui.FieldLayout( widgetInstance, {
+		align: 'top',
+		classes: [ 'welcomesurvey-languages-uls' ],
+		label: mw.message( 'welcomesurvey-question-languages-label' ).text(),
+		help: mw.message( 'welcomesurvey-question-languages-help' )
+			.params( [ mw.language.convertNumber( config.languageMax ) ] )
+			.text(),
+		helpInline: true
+	} );
+	fieldLayout.$element.insertBefore( '.welcomesurvey-mentor-info' );
 }() );
