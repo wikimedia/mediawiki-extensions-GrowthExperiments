0a3b08fa777e10453b3293703d300f8d112cdd93
Gergő Tisza <tgr.huwiki@gmail.com>
2020-11-06 20:31:57 -0800

    Use LinkTarget instead of TitleValue in AddLinkSuggestionsHandler
    Change-Id: I79aa0955f634a074320ac7a6b2b4936ce69d59dd

--- includes/Rest/Handler/AddLinkSuggestionsHandler.php
+++ includes/Rest/Handler/AddLinkSuggestionsHandler.php
@@ -3,12 +3,12 @@
 namespace GrowthExperiments\Rest\Handler;
 
 use GrowthExperiments\NewcomerTasks\AddLink\LinkRecommendationProvider;
+use MediaWiki\Linker\LinkTarget;
 use MediaWiki\ParamValidator\TypeDef\TitleDef;
 use MediaWiki\Rest\Response;
 use MediaWiki\Rest\SimpleHandler;
 use Status;
 use StatusValue;
-use TitleValue;
 use Wikimedia\ParamValidator\ParamValidator;
 
 /**
@@ -30,10 +30,10 @@ class AddLinkSuggestionsHandler extends SimpleHandler {
 
 	/**
 	 * Entry point.
-	 * @param TitleValue $title
+	 * @param LinkTarget $title
 	 * @return Response|mixed A Response or a scalar passed to ResponseFactory::createFromReturnValue
 	 */
-	public function run( TitleValue $title ) {
+	public function run( LinkTarget $title ) {
 		$recommendation = $this->linkRecommendationProvider->get( $title );
 		if ( $recommendation instanceof StatusValue ) {
 			$error = Status::wrap( $recommendation )->getWikiText();
