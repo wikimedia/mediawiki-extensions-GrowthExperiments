d0311ce11460a4eba0d817438e82d7d289ba48f9
Gergő Tisza <tgr.huwiki@gmail.com>
2020-12-21 03:19:39 -0800

    Track task type alongside with target page ID, step 1
    Prepare for updating the tracking cache format so it can include
    task type IDs. To make the change rollback-safe, in the first
    step we don't actually change the format but make the code handle
    the future format as well as the current one.
    
    Also fix the issue where expiry was handled on a per-user level
    so tasks never expired as long as the user kept visiting other
    tasks.
    
    Bug: T269366
    Bug: T270639
    Change-Id: Id322402d9ae4a56b1f8aece149a5c3bc5ab4f886

--- includes/NewcomerTasks/Tracker/CacheStorage.php
+++ includes/NewcomerTasks/Tracker/CacheStorage.php
@@ -4,6 +4,7 @@ namespace GrowthExperiments\NewcomerTasks\Tracker;
 
 use BagOStuff;
 use MediaWiki\User\UserIdentity;
+use MWTimestamp;
 
 class CacheStorage {
 
@@ -33,7 +34,8 @@ class CacheStorage {
 		return $this->cache->merge(
 			$this->getCacheKey(),
 			function ( BagOStuff $cache, $key, $oldVal ) use( $pageId ) {
-				return array_unique( array_merge( [ $pageId ], $oldVal ?: [] ) );
+				$denormalizedOldVal = array_keys( $this->normalizeCacheData( $oldVal ) );
+				return array_unique( array_merge( [ $pageId ], $denormalizedOldVal ) );
 			},
 			$this->cache::TTL_WEEK,
 			1,
@@ -42,14 +44,43 @@ class CacheStorage {
 	}
 
 	/**
-	 * @return array []int
-	 *   Array of page IDs that the user has visited via clicks in the Suggested Edits module.
+	 * @return int[] Array of page IDs that the user has visited via clicks in the
+	 *   Suggested Edits module.
 	 */
 	public function get(): array {
-		return $this->cache->get( $this->getCacheKey() ) ?: [];
+		$cacheData = $this->normalizeCacheData( $this->cache->get( $this->getCacheKey() ) );
+		return array_keys( $cacheData );
 	}
 
 	private function getCacheKey() {
 		return $this->cache->makeKey( self::CACHE_CLASS, $this->userIdentity->getId() );
 	}
+
+	/**
+	 * Make sure the cache data is in a standard format. Deals with cache migrations, and also
+	 * handles per-page expiry.
+	 * @param array<int,int|array>|false $cacheData Raw cache data.
+	 * @return array<int,array> An array of page ID => data. Data fields:
+	 *   - type: task type ID. Always present; for the migration period can be null.
+	 *   - expires: per-item expiry time, as a unix timestamp. Always present.
+	 */
+	private function normalizeCacheData( $cacheData ): array {
+		if ( $cacheData === false ) {
+			return [];
+		}
+
+		$newCacheData = [];
+		$expires = MWTimestamp::now( TS_UNIX ) + $this->cache::TTL_WEEK;
+		foreach ( $cacheData as $key => $val ) {
+			if ( is_numeric( $val ) ) {
+				$newCacheData[$val] = [ 'type' => null, 'expires' => $expires ];
+			} elseif ( $val['expires'] < MWTimestamp::now( TS_UNIX ) ) {
+				// expired, skip
+			} else {
+				$newCacheData[$key] = $val;
+			}
+		}
+		return $newCacheData;
+	}
+
 }
--- tests/phpunit/unit/CacheStorageTest.php
+++ tests/phpunit/unit/CacheStorageTest.php
@@ -3,13 +3,21 @@
 namespace GrowthExperiments\Tests;
 
 use GrowthExperiments\NewcomerTasks\Tracker\CacheStorage;
+use HashBagOStuff;
 use MediaWiki\User\UserIdentityValue;
 use MediaWikiUnitTestCase;
+use MWTimestamp;
+use Wikimedia\TestingAccessWrapper;
 
 /**
  * @coversDefaultClass \GrowthExperiments\NewcomerTasks\Tracker\CacheStorage
  */
 class CacheStorageTest extends MediaWikiUnitTestCase {
+
+	protected function tearDown(): void {
+		parent::tearDown();
+		MWTimestamp::setFakeTime( false );
+	}
 
 	/**
 	 * @covers ::__construct
@@ -22,36 +30,71 @@ class CacheStorageTest extends MediaWikiUnitTestCase {
 	}
 
 	/**
+	 * @covers ::get
 	 * @covers ::set
-	 * @covers ::getCacheKey
 	 */
-	public function testSetSuccess() {
+	public function testGetSet() {
 		$cacheStorage = new CacheStorage(
-			new \EmptyBagOStuff(),
+			new HashBagOStuff(),
 			new UserIdentityValue( 1, 'Foo', 0 )
 		);
+		$this->assertSame( [], $cacheStorage->get() );
 		$this->assertTrue( $cacheStorage->set( 42 ) );
+		$this->assertSame( [ 42 ], $cacheStorage->get() );
+		$this->assertTrue( $cacheStorage->set( 43 ) );
+		$this->assertArrayEquals( [ 42, 43 ], $cacheStorage->get() );
+		$this->assertTrue( $cacheStorage->set( 43 ) );
+		$this->assertArrayEquals( [ 42, 43 ], $cacheStorage->get() );
 	}
 
 	/**
-	 * @covers ::set
 	 * @covers ::get
-	 * @covers ::getCacheKey
+	 * @covers ::set
 	 */
-	public function testGet() {
-		$cacheMock = $this->getMockBuilder( \BagOStuff::class )
-			->disableOriginalConstructor()
-			->getMock();
-		$cacheMock->method( 'get' )
-			->willReturn( [ 42 ] );
-		$cacheMock->method( 'merge' )
-			->willReturn( true );
+	public function testExpiry() {
+		// this will only work after the cache format migration
+		$this->markTestSkipped();
 		$cacheStorage = new CacheStorage(
-			$cacheMock,
+			new HashBagOStuff(),
 			new UserIdentityValue( 1, 'Foo', 0 )
 		);
-		$cacheStorage->set( 42 );
-		$this->assertEquals( [ 42 ], $cacheStorage->get() );
+		MWTimestamp::setFakeTime( '2000-01-01 00:00:00' );
+		$this->assertTrue( $cacheStorage->set( 42 ) );
+		MWTimestamp::setFakeTime( '2000-01-05 00:00:00' );
+		$this->assertTrue( $cacheStorage->set( 43 ) );
+		$this->assertArrayEquals( [ 42, 43 ], $cacheStorage->get() );
+		MWTimestamp::setFakeTime( '2000-01-10 00:00:00' );
+		$this->assertArrayEquals( [ 43 ], $cacheStorage->get() );
+		$this->assertTrue( $cacheStorage->set( 44 ) );
+		$this->assertArrayEquals( [ 43, 44 ], $cacheStorage->get() );
+		MWTimestamp::setFakeTime( '2000-01-15 00:00:00' );
+		$this->assertArrayEquals( [ 44 ], $cacheStorage->get() );
+		MWTimestamp::setFakeTime( '2000-01-20 00:00:00' );
+		$this->assertArrayEquals( [], $cacheStorage->get() );
+	}
+
+	/**
+	 * Test that things work with the future data format [ page id => data ].
+	 * This format will be added by a later patch, and the code needs to survive a rollback.
+	 * @covers ::get
+	 * @covers ::set
+	 */
+	public function testGetSetWithFutureData() {
+		$bag = new HashBagOStuff();
+		$cacheStorage = new CacheStorage(
+			$bag,
+			new UserIdentityValue( 1, 'Foo', 0 )
+		);
+		$cacheKey = TestingAccessWrapper::newFromObject( $cacheStorage )->getCacheKey();
+		$bag->set( $cacheKey, [
+			42 => [ 'type' => 'Foo', 'expires' => time() + 10000 ],
+			43 => [ 'type' => 'Bar', 'expires' => time() + 10000 ],
+		] );
+		$this->assertEquals( [ 42, 43 ], $cacheStorage->get() );
+		$this->assertTrue( $cacheStorage->set( 44 ) );
+		$this->assertArrayEquals( [ 42, 43, 44 ], $cacheStorage->get() );
+		$this->assertTrue( $cacheStorage->set( 45 ) );
+		$this->assertArrayEquals( [ 42, 43, 44, 45 ], $cacheStorage->get() );
 	}
 
 }
