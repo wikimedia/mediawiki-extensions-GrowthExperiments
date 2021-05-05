8fd76100cc0fe89c9dc5e81c4bbd398992573b2a
MewOphaswongse <mophaswongse@wikimedia.org>
2021-05-05 10:54:56 -0700

    Add a link: open help panel's suggested-edits panel instead of home
    * Define default panel based on suggestedEditSession.taskType
    * Start setting up unit tests for HelpPanelProcessDialog
    
    Bug: T278488
    Change-Id: I0aa9621a8bf4df0f00608d0f3c7e087bf757990c

--- tests/qunit/index.js
+++ tests/qunit/index.js
@@ -4,6 +4,7 @@ QUnit.dump.maxDepth = 999;
 
 require( './utils/ext.growthExperiments.Utils.test.js' );
 require( './help/ext.growthExperiments.HelpPanelLogger.test.js' );
+require( './help/ext.growthExperiments.HelpPanelProcessDialog.test.js' );
 require( './help/ext.growthExperiments.HelpPanelProcessDialog.SwitchEditorPanel.test.js' );
 require( './homepage/ext.growthExperiments.Homepage.Logger.test.js' );
 require( './homepage/suggestededits/ext.growthExperiments.Homepage.SuggestedEditPagerWidget.test.js' );
