1d307dfb889642de740073749995c0f5da8bf064
Martin Urbanec <martin.urbanec@wikimedia.cz>
2021-06-01 12:54:10 +0200

    AddLinkSaveDialogMixin.js: Use mw.language.convertNumber to convert numbers
    See also T274379.
    
    Bug: T284046
    Change-Id: I0a27789cee4e87b7899a44d1d1fb2b0f8077c728

--- modules/homepage/addlink/AddLinkSaveDialogMixin.js
+++ modules/homepage/addlink/AddLinkSaveDialogMixin.js
@@ -118,7 +118,9 @@ AddLinkSaveDialogMixin.prototype.getSetupProcess = function ( data ) {
 		this.updateSummary( annotationStates );
 		this.editSummaryInput.setValue(
 			mw.message( 'growthexperiments-addlink-summary-summary' ).params( [
-				acceptedCount, rejectedCount, skippedCount
+				mw.language.convertNumber( acceptedCount ),
+				mw.language.convertNumber( rejectedCount ),
+				mw.language.convertNumber( skippedCount )
 			] ).text()
 		);
 		if ( this.checkboxesByName.wpWatchthis ) {
