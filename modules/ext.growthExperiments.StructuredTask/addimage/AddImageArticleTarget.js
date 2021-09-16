d49ff2dd06f2ee1ed6dc56c81ef38acc53d66e6a
Gergő Tisza <tgr.huwiki@gmail.com>
2021-09-15 23:57:49 -0700

    Add an image: Save placeholder
    Show save dialog after clicking accept/reject, and do the
    saving.
    
    Much of this will be handled differently in the final version,
    but until then it facilitates testing of the image insertion
    and submission/invalidation logic.
    
    Change-Id: I644e4dd1a0045c329c39648366597becaadc2507

--- modules/ext.growthExperiments.StructuredTask/addimage/AddImageArticleTarget.js
+++ modules/ext.growthExperiments.StructuredTask/addimage/AddImageArticleTarget.js
@@ -1,3 +1,25 @@
+var suggestedEditSession = require( 'ext.growthExperiments.SuggestedEditSession' ).getInstance();
+
+/**
+ * @typedef mw.libs.ge.ImageRecommendationImage
+ * @property {string} image Image filename in unprefixed DBkey format.
+ * @property {string} source Recommendation source; one of 'wikidata', 'wikipedia', 'commons'.
+ * @property {string[]} projects Lists of projects (as wiki IDs) the recommendation is from.
+ *   Only used when the source is 'wikipedia'.
+ * @property {Object} metadata
+ * @property {string} metadata.description Image description (sanitized HTML).
+ * @property {string} metadata.descriptionUrl File description page URL.
+ * @property {string} metadata.fullUrl URL of full-sized image.
+ * @property {string} metadata.thumbUrl URL of image thumbnail used in the toolbar dialog.
+ * @property {int} metadata.originalWidth Width of original image in pixels.
+ * @property {int} metadata.originalHeight Height of original image in pixels.
+ */
+/**
+ * @typedef mw.libs.ge.ImageRecommendation
+ * @property {ImageRecommendationImage[]} images Recommended images.
+ * @property {string} datasetId Dataset version ID.
+ */
+
 /**
  * Mixin for a ve.init.mw.ArticleTarget instance. Used by AddImageDesktopArticleTarget and
  * AddImageMobileArticleTarget.
@@ -16,14 +38,31 @@ AddImageArticleTarget.prototype.afterSurfaceReady = function () {
 	this.getSurface().executeCommand( 'recommendedImage' );
 };
 
+/** @inheritDoc **/
 AddImageArticleTarget.prototype.hasEdits = function () {
-	// TODO: Actual implementation
-	return false;
+	var accepted = this.getSurface().geRecommendationAccepted;
+	return accepted === true;
 };
 
+/** @inheritDoc **/
 AddImageArticleTarget.prototype.hasReviewedSuggestions = function () {
-	// TODO: Actual implementation
-	return false;
+	var accepted = this.getSurface().geRecommendationAccepted;
+	return accepted === true || accepted === false;
+};
+
+/** @inheritDoc **/
+AddImageArticleTarget.prototype.save = function ( doc, options, isRetry ) {
+	/** @var {mw.libs.ge.ImageRecommendation} taskData */
+	var taskData = suggestedEditSession.taskData,
+		accepted = this.getSurface().geRecommendationAccepted;
+
+	options.plugins = 'ge-task-image-recommendation';
+	// This data will be processed in HomepageHooks::onVisualEditorApiVisualEditorEditPostSaveHook
+	options[ 'data-ge-task-image-recommendation' ] = JSON.stringify( {
+		filename: taskData.images[ 0 ].image,
+		accepted: accepted
+	} );
+	return this.constructor.super.prototype.save.call( this, doc, options, isRetry );
 };
 
 module.exports = AddImageArticleTarget;
