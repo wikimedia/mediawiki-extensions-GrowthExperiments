5e39f7cc82872b24cecfbeb4c5c0af96a6642544
Kosta Harlan <kharlan@wikimedia.org>
2021-09-21 14:01:29 +0200

    phpbench: tests for LinkRecommendationFilter and Special:Homepage
    This patch adds a phpbench (https://github.com/phpbench/phpbench) test
    for the link recommendation filter and Special:Homepage rendering.
    
    To set up, run "composer update" in the GrowthExperiments directory.
    
    Then from the GrowthExperiments directory, run:
    
     vendor/bin/phpbench run tests/Benchmark --revs=10
    
    Bug: T291549
    Change-Id: I8f1807f1d43aaecdaab49913e3b7c03f8c6a160f

---
+++ tests/Benchmark/LinkRecommendationFilterBench.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace GrowthExperiments\Tests\Benchmark;
+
+$path = dirname( __DIR__, 4 );
+
+if ( getenv( 'MW_INSTALL_PATH' ) !== false ) {
+	$path = getenv( 'MW_INSTALL_PATH' );
+}
+
+require_once $path . '/includes/Setup.php';
+
+require_once "GrowthExperimentsBench.php";
+
+class LinkRecommendationFilterBench extends GrowthExperimentsBench {
+
+	// phpcs:disable MediaWiki.Commenting.FunctionAnnotations.UnrecognizedAnnotation
+
+	/**
+	 * @BeforeMethods ("setUpLinkRecommendation")
+	 * @AfterMethods ("tearDownLinkRecommendation")
+	 * @Assert("mode(variant.time.avg) < 100000 microseconds +/- 10%")
+	 */
+	public function benchFilter() {
+		$this->linkRecommendationFilter->filter( $this->tasks );
+	}
+
+	// phpcs:enable MediaWiki.Commenting.FunctionAnnotations.UnrecognizedAnnotation
+}
---
+++ tests/Benchmark/SpecialHomepageBench.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace GrowthExperiments\Tests\Benchmark;
+
+use GlobalVarConfig;
+use GrowthExperiments\GrowthExperimentsServices;
+use GrowthExperiments\Specials\SpecialHomepage;
+use MediaWiki\MediaWikiServices;
+use SpecialPage;
+
+$path = dirname( __DIR__, 4 );
+
+if ( getenv( 'MW_INSTALL_PATH' ) !== false ) {
+	$path = getenv( 'MW_INSTALL_PATH' );
+}
+
+require_once $path . '/includes/Setup.php';
+
+require_once "GrowthExperimentsBench.php";
+
+class SpecialHomepageBench extends GrowthExperimentsBench {
+
+	/** @var SpecialHomepage */
+	private $homepage;
+
+	public function setUp() {
+		$services = MediaWikiServices::getInstance();
+		$growthExperimentsServices = GrowthExperimentsServices::wrap( $services );
+		$userOptionManager = $services->getUserOptionsManager();
+		$this->homepage = new SpecialHomepage(
+			$growthExperimentsServices->getHomepageModuleRegistry(),
+			$services->getStatsdDataFactory(),
+			$services->getPerDbNameStatsdDataFactory(),
+			$growthExperimentsServices->getExperimentUserManager(),
+			// This would normally be wiki-powered config, but
+			// there is no need to test this
+			GlobalVarConfig::newInstance(),
+			$userOptionManager,
+			$services->getTitleFactory()
+		);
+		$context = new \DerivativeContext( \RequestContext::getMain() );
+		$testUser = $services->getUserFactory()->newFromId( 1 );
+		$context->setUser( $testUser );
+		$userOptionManager->setOption( $testUser, 'growthexperiments-homepage-enable', 1 );
+		$userOptionManager->saveOptions( $testUser );
+		// Needed for StartEmail.php and other code that may look for the context title.
+		$context->setTitle( SpecialPage::getTitleFor( 'Homepage' ) );
+		$this->homepage->setContext( $context );
+	}
+
+	// phpcs:disable MediaWiki.Commenting.FunctionAnnotations.UnrecognizedAnnotation
+
+	/**
+	 * @BeforeMethods ("setUp", "setUpLinkRecommendation" )
+	 * @AfterMethods ("tearDownLinkRecommendation" )
+	 * @Assert("mode(variant.time.avg) < 700000 microseconds +/- 10%")
+	 */
+	public function benchExecute() {
+		$this->homepage->execute();
+	}
+
+	// phpcs:enable MediaWiki.Commenting.FunctionAnnotations.UnrecognizedAnnotation
+}
