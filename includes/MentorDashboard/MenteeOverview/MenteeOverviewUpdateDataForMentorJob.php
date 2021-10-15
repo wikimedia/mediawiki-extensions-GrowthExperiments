8d0c1ab3f4bc1f246ebdc8235eb60059b600d984
Martin Urbanec <martin.urbanec@wikimedia.cz>
2021-10-16 00:24:56 +0200

    Add ApiMentorDashboardUpdateData
    In a future commit, this will be used from
    mentor dashboard's frontend, allowing the mentor
    to schedule updates of the mentee overview module.
    
    The API is tightly rate limited: we only let mentors
    to request one update each 2 hours. This limit cannot
    be bypassed even by admins, as the update is potentially
    expensive.
    
    Bug: T293454
    Change-Id: Ia4862f8f38282d7a932142d7b77e0135c9ff5fe9

---
+++ includes/MentorDashboard/MenteeOverview/MenteeOverviewUpdateDataForMentorJob.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace GrowthExperiments\MentorDashboard\MenteeOverview;
+
+use GenericParameterJob;
+use GrowthExperiments\GrowthExperimentsServices;
+use Job;
+use MediaWiki\MediaWikiServices;
+use MediaWiki\User\UserIdentityLookup;
+
+/**
+ * Job to update data shown by the mentor dashboard for a given mentor
+ *
+ * This job is started by the mentor, if they wish to do an one-off update
+ * of the data.
+ *
+ * The following job parameters are required:
+ * 	- mentorId: user ID of the mentor
+ */
+class MenteeOverviewUpdateDataForMentorJob extends Job implements GenericParameterJob {
+
+	/** @var MenteeOverviewDataUpdater */
+	private $menteeOverviewDataUpdater;
+
+	/** @var UserIdentityLookup */
+	private $userIdentityLookup;
+
+	/**
+	 * @inheritDoc
+	 */
+	public function __construct( $params = null ) {
+		parent::__construct( 'menteeOverviewUpdateDataForMentor', $params );
+		$this->removeDuplicates = true;
+
+		// Init services
+		$services = MediaWikiServices::getInstance();
+		$this->menteeOverviewDataUpdater = GrowthExperimentsServices::wrap( $services )
+			->getMenteeOverviewDataUpdater();
+		$this->userIdentityLookup = $services->getUserIdentityLookup();
+	}
+
+	public function run() {
+		$mentor = $this->userIdentityLookup->getUserIdentityByUserId( $this->params['mentorId'] );
+		if ( !$mentor ) {
+			// invalid ID passed
+			return true;
+		}
+
+		$this->menteeOverviewDataUpdater->updateDataForMentor(
+			$mentor
+		);
+		return true;
+	}
+}
