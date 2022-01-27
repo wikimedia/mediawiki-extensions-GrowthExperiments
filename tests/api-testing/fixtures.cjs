9630c366332a93b1a96153a3f34cbe048addac17
Kosta Harlan <kharlan@wikimedia.org>
2022-01-27 11:31:59 +0100

    api-testing: Load LocalSettings.php from Selenium tests
    This loads NewcomerTasks and functional suggested edits configuration,
    which should allow for us to run the tests in CI.
    
    Also switch on the --parallel execution flag (T298735).
    
    Bug: T253015
    Change-Id: I429a6bdfac1ad895b35dce2c0bff347fcc0f3dee

---
+++ tests/api-testing/fixtures.cjs
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const path = require("path");
+const process = require("process");
+const childProcess = require("child_process");
+const ip = path.resolve( __dirname + '/../../../../' );
+const localSettings = fs.readFileSync( path.resolve( ip + '/LocalSettings.php' ) );
+
+exports.mochaGlobalSetup = async function() {
+	console.log( 'Setting up modified LocalSettings.php' );
+	fs.writeFileSync( path.resolve( ip + '/LocalSettings.php' ),
+		// Load the service overrides
+		localSettings + `
+if ( file_exists( "$IP/extensions/GrowthExperiments/tests/selenium/fixtures/GrowthExperiments.LocalSettings.php" ) ) {
+    require_once "$IP/extensions/GrowthExperiments/tests/selenium/fixtures/GrowthExperiments.LocalSettings.php";
+}
+` );
+	// This is needed in Quibble + Apache (T225218) because we use supervisord to control
+	// the php-fpm service, and with supervisord you need to restart the php-fpm service
+	// in order to load updated php code.
+	if ( process.env['QUIBBLE_APACHE'] ) {
+		childProcess.spawnSync(
+			'service',
+			['php7.2-fpm', 'restart']
+		);
+		// Super ugly hack: Run this twice because sometimes the first invocation hangs.
+		childProcess.spawnSync(
+			'service',
+			['php7.2-fpm', 'restart']
+		);
+	}
+};
+
+exports.mochaGlobalTeardown = async function() {
+	console.log( 'Restoring LocalSettings.php');
+	fs.writeFileSync( path.resolve( ip + '/LocalSettings.php' ), localSettings );
+};
