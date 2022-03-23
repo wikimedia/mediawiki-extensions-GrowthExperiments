9aeb62fd8398045a2dbfaf9a94ae39261095b62f
daniel <dkinzler@wikimedia.org>
2022-03-23 14:36:20 +0100

    Use MW_QUIBBLE_CI constant
    $wgWikimediaJenkinsCI is deprecated, MW_QUIBBLE_CI should be used
    instead.
    
    Change-Id: Ib59819c2b1642e02c58521891756aadd824ef7ca

--- tests/selenium/fixtures/GrowthExperiments.LocalSettings.php
+++ tests/selenium/fixtures/GrowthExperiments.LocalSettings.php
@@ -67,7 +67,7 @@ $wgUseInstantCommons = true;
 $wgGELinkRecommendationServiceUrl = 'https://api.wikimedia.org/service/linkrecommendation';
 
 // Conditionally load Parsoid in CI
-if ( $wgWikimediaJenkinsCI && !is_dir( "$IP/services/parsoid" ) ) {
+if ( defined( 'MW_QUIBBLE_CI' ) && !is_dir( "$IP/services/parsoid" ) ) {
 	$PARSOID_INSTALL_DIR = "$IP/vendor/wikimedia/parsoid";
 	wfLoadExtension( 'Parsoid', "$PARSOID_INSTALL_DIR/extension.json" );
 }
