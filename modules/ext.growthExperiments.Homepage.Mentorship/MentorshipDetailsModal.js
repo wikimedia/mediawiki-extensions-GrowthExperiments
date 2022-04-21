100945253bc63b60389da8bf3cacfcfe3543338e
Martin Urbanec <martin.urbanec@wikimedia.cz>
2022-04-21 15:32:17 +0200

    Mentorship: Add About mentorship dialog
    Bug: T292627
    Change-Id: If554d2a64cda74da54d7c227a64fd6bed4725d43

---
+++ modules/ext.growthExperiments.Homepage.Mentorship/MentorshipDetailsModal.js
@@ -0,0 +1,60 @@
+( function () {
+	'use strict';
+
+	function MentorshipDetailsModal( config ) {
+		MentorshipDetailsModal.super.call( this, $.extend( {}, config, {
+			size: 'medium'
+		} ) );
+	}
+	OO.inheritClass( MentorshipDetailsModal, OO.ui.ProcessDialog );
+
+	MentorshipDetailsModal.static.name = 'mentorshipDetailsModal';
+	MentorshipDetailsModal.static.title = mw.msg( 'growthexperiments-homepage-mentorship-about-header' );
+	MentorshipDetailsModal.static.actions = [
+		{
+			action: 'done',
+			label: mw.msg( 'growthexperiments-homepage-mentorship-about-done' ),
+			flags: [ 'primary', 'progressive' ]
+		}
+	];
+
+	MentorshipDetailsModal.prototype.onOptoutButtonClicked = function () {
+		var dialog = this;
+		setTimeout( function () {
+			dialog.emit( 'optout' );
+		}, 100 );
+
+		this.close( { action: 'done' } );
+	};
+
+	MentorshipDetailsModal.prototype.initialize = function () {
+		MentorshipDetailsModal.super.prototype.initialize.call( this );
+
+		var optoutBtn = new OO.ui.ButtonWidget( {
+			label: mw.msg( 'growthexperiments-homepage-mentorship-ellipsis-menu-optout' )
+		} );
+		optoutBtn.connect( this, {
+			click: [ 'onOptoutButtonClicked' ]
+		} );
+
+		this.content = new OO.ui.PanelLayout( { padded: true, expanded: false } );
+		this.content.$element.append(
+			$( '<h4>' ).text( mw.msg( 'growthexperiments-homepage-mentorship-about-subheader-mentor' ) ),
+			$( '<p>' ).text( mw.msg( 'growthexperiments-homepage-mentorship-about-mentor-par1' ) ),
+			$( '<p>' ).text( mw.msg( 'growthexperiments-homepage-mentorship-about-mentor-par2' ) ),
+			$( '<h4>' ).text( mw.msg( 'growthexperiments-homepage-mentorship-about-subheader-optout' ) ),
+			$( '<p>' ).text( mw.msg( 'growthexperiments-homepage-mentorship-about-optout-par1' ) ),
+			optoutBtn.$element
+		);
+		this.$body.append( this.content.$element );
+	};
+
+	MentorshipDetailsModal.prototype.getActionProcess = function ( action ) {
+		var dialog = this;
+
+		return new OO.ui.Process( function () {
+			dialog.close( { action: action } );
+		} );
+	};
+	module.exports = MentorshipDetailsModal;
+}() );
