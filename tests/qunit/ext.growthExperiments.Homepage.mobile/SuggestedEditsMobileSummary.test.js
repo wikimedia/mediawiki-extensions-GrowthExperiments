d919830e70d26ac05311bbf11e037e6483c427f2
MewOphaswongse <mophaswongse@wikimedia.org>
2022-05-09 14:25:38 -0700

    Change how suggested edits is set up in ext.growthExperiments.Homepage.mobile
    * Create SuggestedEditsMobileSummary module
    * Reorganize ext.growthExperiments.Homepage.mobile's entry point by separating out the logic for setting up the overlays and for setting up suggested edits
    * Only re-render SuggestedEditsMobileSummary when the suggested edits overlay is closed (instead of anytime the task queue changes in ext.growthExperiments.Homepage.SuggestedEdits)
    * Move LastDayEditsWidget and TaskPreviewWidget to ext.growthExperiments.Homepage.mobile since these are only used in this module
    
    Bug: T307961
    Change-Id: I8fedb14fb1d07bfe95f0fe890c6049ed4349bf2f

---
+++ tests/qunit/ext.growthExperiments.Homepage.mobile/SuggestedEditsMobileSummary.test.js
@@ -0,0 +1,105 @@
+'use strict';
+
+const NewcomerTasksStore = require( '../../../modules/ext.growthExperiments.DataStore/NewcomerTasksStore.js' );
+const SuggestedEditsMobileSummary = require( '../../../modules/ext.growthExperiments.Homepage.mobile/SuggestedEditsMobileSummary.js' );
+const rootStore = require( '../__mocks__/store.js' );
+const store = {
+	newcomerTasks: new NewcomerTasksStore( rootStore ),
+	CONSTANTS: require( '../../../modules/ext.growthExperiments.DataStore/constants.js' )
+};
+
+QUnit.module( 'ext.growthExperiments.Homepage.mobile/SuggestedEditsMobileSummary.js', QUnit.newMwEnvironment( {
+	config: {
+		homepagemodules: {
+			'suggested-edits': {
+				'task-preview': {
+					title: 'Article title'
+				}
+			}
+		},
+		'wgGEHomepageModuleActionData-suggested-edits': {
+			taskCount: 1
+		}
+	},
+	beforeEach() {
+		this.sandbox.stub( mw.language, 'convertNumber', function ( num ) {
+			return String( num );
+		} );
+	}
+} ) );
+
+QUnit.test( 'should show LastDayEditsWidget if there is no task preview ', function ( assert ) {
+	const done = assert.async();
+	const getConfigStub = this.sandbox.stub( mw.config, 'get' );
+	getConfigStub.withArgs( 'homepagemodules' ).returns( {
+		'suggested-edits': {
+		}
+	} );
+	getConfigStub.withArgs( 'wgGEHomepageModuleActionData-suggested-edits' ).returns( {
+		taskCount: 0,
+		editCount: 10
+	} );
+	const module = new SuggestedEditsMobileSummary( {
+		$element: $( '<div>' ),
+		newcomerTaskLogger: { log() {} },
+		homepageModuleLogger: { log() {} }
+	}, store );
+	module.initialize().then( () => {
+		assert.true( [ ...module.$content[ 0 ].classList ].includes( 'growthexperiments-last-day-edits-widget' ) );
+		done();
+	} );
+} );
+
+QUnit.test( 'should hide page views in SmallTaskCard if task preview is available', function ( assert ) {
+	const done = assert.async();
+	const task = {
+		title: 'Article title',
+		difficulty: 'easy',
+		tasktype: 'copyedit',
+		pageviews: 200
+	};
+	this.sandbox.stub( store.newcomerTasks.api, 'getExtraDataFromPcs', function ( taskData ) {
+		$.extend( taskData, task );
+		return $.Deferred().resolve( task ).promise();
+	} );
+	const module = new SuggestedEditsMobileSummary( {
+		$element: $( '<div>' ),
+		newcomerTaskLogger: { log() {} },
+		homepageModuleLogger: { log() {} }
+	}, store );
+	module.initialize().then( () => {
+		const $content = module.$content;
+		assert.strictEqual( $content.find( '.mw-ge-small-task-card-pageviews' ).text(), '' );
+		assert.strictEqual( $content.find( '.mw-ge-small-task-card-title' ).text(), task.title );
+		done();
+	} );
+} );
+
+QUnit.test( 'should show LastDayEditsWidget for updateUiBasedOnState if there is no current task', function ( assert ) {
+	const module = new SuggestedEditsMobileSummary( {
+		$element: $( '<div>' ),
+		newcomerTaskLogger: { log() {} },
+		homepageModuleLogger: { log() {} }
+	}, store );
+	store.newcomerTasks.currentTaskIndex = -1;
+	module.updateUiBasedOnState();
+	assert.true( [ ...module.$content[ 0 ].classList ].includes( 'growthexperiments-last-day-edits-widget' ) );
+} );
+
+QUnit.test( 'should show the preview for the current task for updateUiBasedOnState', function ( assert ) {
+	const module = new SuggestedEditsMobileSummary( {
+		$element: $( '<div>' ),
+		newcomerTaskLogger: { log() {} },
+		homepageModuleLogger: { log() {} }
+	}, store );
+	const task = {
+		title: 'Article title',
+		difficulty: 'easy',
+		tasktype: 'copyedit',
+		pageviews: 200
+	};
+	store.newcomerTasks.taskQueue = [ task ];
+	store.newcomerTasks.currentTaskIndex = 0;
+	module.updateUiBasedOnState();
+	assert.strictEqual( module.$content.find( '.mw-ge-small-task-card-title' ).text(), task.title );
+} );
