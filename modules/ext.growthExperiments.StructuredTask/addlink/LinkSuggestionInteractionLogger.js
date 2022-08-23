b31fa6c6f06e2d71419a24982c72f4caefe065a3
Gergő Tisza <tgr.huwiki@gmail.com>
2022-08-23 15:02:38 -0700

    Bump link_suggestion_interaction schema version
    Bug: T314899
    Change-Id: I028f954711875c948d4245a3f1a72cc2936016ef

--- modules/ext.growthExperiments.StructuredTask/addlink/LinkSuggestionInteractionLogger.js
+++ modules/ext.growthExperiments.StructuredTask/addlink/LinkSuggestionInteractionLogger.js
@@ -3,7 +3,7 @@
 ( function () {
 
 	var StructuredTaskLogger = require( '../StructuredTaskLogger.js' ),
-		schema = '/analytics/mediawiki/structured_task/article/link_suggestion_interaction/1.7.0',
+		schema = '/analytics/mediawiki/structured_task/article/link_suggestion_interaction/1.7.1',
 		streamName = 'mediawiki.structured_task.article.link_suggestion_interaction';
 
 	/**
