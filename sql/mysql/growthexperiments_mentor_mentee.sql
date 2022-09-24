f62d3ef95e29f050e03e2e4705337ac0971f5d32
Martin Urbanec <martin.urbanec@wikimedia.cz>
2022-09-24 12:23:36 +0200

    growthexperiments_mentor_mentee: Add gemm_mentee_is_active
    This field will be used by MentorFilterHooks
    to only feed recently active users to the recent changes
    query. This makes errors like T293182 less likely to happen.
    
    Bug: T318457
    Change-Id: I48f26221746379dcb9148c2b1c06b435d204bb96

--- sql/mysql/growthexperiments_mentor_mentee.sql
+++ sql/mysql/growthexperiments_mentor_mentee.sql
@@ -6,7 +6,10 @@ CREATE TABLE /*_*/growthexperiments_mentor_mentee (
   gemm_mentee_id INT UNSIGNED NOT NULL,
   gemm_mentor_role VARBINARY(255) NOT NULL,
   gemm_mentor_id INT UNSIGNED NOT NULL,
-  INDEX gemm_mentor (gemm_mentor_id),
+  gemm_mentee_is_active TINYINT(1) DEFAULT 1 NOT NULL,
+  INDEX gemm_mentor (
+    gemm_mentor_id, gemm_mentee_is_active
+  ),
   PRIMARY KEY(
     gemm_mentee_id, gemm_mentor_role
   )
