ce9e8e202404848d0990a3b5dd8a0e47e653bbd5
Sergio Gimeno <sgimeno@wikimedia.org>
2022-09-30 16:04:20 +0200

    User impact: remove unused dependency vuex
    Bug: T310666
    Change-Id: I6c6c89ad5f81d9eab1bed89563ceb254270bb780

--- modules/ext.growthExperiments.Homepage.NewImpact/index.js
+++ modules/ext.growthExperiments.Homepage.NewImpact/index.js
@@ -1,9 +1,7 @@
 ( function () {
 	'use strict';
 	const Vue = require( 'vue' );
-	const Vuex = require( 'vuex' );
 	const NewImpact = require( './components/NewImpact.vue' );
-	Vue.use( Vuex );
 	Vue.createMwApp( NewImpact )
 		.mount( '#new-impact-vue-root' );
 }() );
