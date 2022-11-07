e0272a68f2a90e17a956e791f03feb20d43b45b8
Kosta Harlan <kharlan@wikimedia.org>
2022-11-07 12:21:55 +0100

    ImpactHooks: Refresh user data after thanks received
    Listen for log entry events of the type/subtype "thanks/thank" and if
    the user is in the impact data cohort, refresh the user's impact data.
    
    I considered creating a dedicated hook in Thanks, but seems like using
    the log entry before publish hook is sufficient.
    
    Bug: T313393
    Change-Id: Ic0ab089f43b422f06399d163843933868a0f7294

--- includes/ImpactHooks.php
+++ includes/ImpactHooks.php
@@ -6,34 +6,45 @@ use Config;
 use DeferredUpdates;
 use GrowthExperiments\UserImpact\UserImpactLookup;
 use GrowthExperiments\UserImpact\UserImpactStore;
+use MediaWiki\Hook\ManualLogEntryBeforePublishHook;
 use MediaWiki\ResourceLoader\Hook\ResourceLoaderRegisterModulesHook;
 use MediaWiki\ResourceLoader\ResourceLoader;
 use MediaWiki\Storage\Hook\PageSaveCompleteHook;
+use MediaWiki\User\UserFactory;
+use MediaWiki\User\UserIdentity;
 use MediaWiki\User\UserOptionsLookup;
 
-class ImpactHooks implements ResourceLoaderRegisterModulesHook, PageSaveCompleteHook {
+class ImpactHooks implements
+	ResourceLoaderRegisterModulesHook,
+	PageSaveCompleteHook,
+	ManualLogEntryBeforePublishHook
+{
 
 	private Config $config;
 	private UserImpactLookup $userImpactLookup;
 	private UserImpactStore $userImpactStore;
 	private UserOptionsLookup $userOptionsLookup;
+	private UserFactory $userFactory;
 
 	/**
 	 * @param Config $config
 	 * @param UserImpactLookup $userImpactLookup
 	 * @param UserImpactStore $userImpactStore
 	 * @param UserOptionsLookup $userOptionsLookup
+	 * @param UserFactory $userFactory
 	 */
 	public function __construct(
 		Config $config,
 		UserImpactLookup $userImpactLookup,
 		UserImpactStore $userImpactStore,
-		UserOptionsLookup $userOptionsLookup
+		UserOptionsLookup $userOptionsLookup,
+		UserFactory $userFactory
 	) {
 		$this->config = $config;
 		$this->userImpactLookup = $userImpactLookup;
 		$this->userImpactStore = $userImpactStore;
 		$this->userOptionsLookup = $userOptionsLookup;
+		$this->userFactory = $userFactory;
 	}
 
 	/**
@@ -66,16 +77,45 @@ class ImpactHooks implements ResourceLoaderRegisterModulesHook, PageSaveComplete
 	/** @inheritDoc */
 	public function onPageSaveComplete( $wikiPage, $user, $summary, $flags, $revisionRecord, $editResult ) {
 		// Refresh the user's impact after they've made an edit.
-		if ( $user->isRegistered() &&
-			$user->equals( $revisionRecord->getUser() ) &&
-			$this->userOptionsLookup->getBoolOption( $user, HomepageHooks::HOMEPAGE_PREF_ENABLE )
+		if ( $this->userIsInImpactDataCohort( $user ) &&
+			$user->equals( $revisionRecord->getUser() )
 		) {
-			DeferredUpdates::addCallableUpdate( function () use ( $user ) {
-				$impact = $this->userImpactLookup->getExpensiveUserImpact( $user );
-				if ( $impact ) {
-					$this->userImpactStore->setUserImpact( $impact );
-				}
-			} );
+			$this->refreshUserImpactDataInDeferredUpdate( $user );
+
 		}
+	}
+
+	/** @inheritDoc */
+	public function onManualLogEntryBeforePublish( $logEntry ): void {
+		if ( $logEntry->getType() === 'thanks' && $logEntry->getSubtype() === 'thank' ) {
+			$recipientUserPage = $logEntry->getTarget();
+			$user = $this->userFactory->newFromName( $recipientUserPage->getDBkey() );
+			if ( $user instanceof UserIdentity && $this->userIsInImpactDataCohort( $user ) ) {
+				$this->refreshUserImpactDataInDeferredUpdate( $user );
+			}
+		}
+	}
+
+	/**
+	 * @param UserIdentity $userIdentity
+	 * @return bool
+	 */
+	private function userIsInImpactDataCohort( UserIdentity $userIdentity ): bool {
+		return $userIdentity->isRegistered() &&
+			// TODO: Also check if user has been active in last 6(?) months.
+			$this->userOptionsLookup->getBoolOption( $userIdentity, HomepageHooks::HOMEPAGE_PREF_ENABLE );
+	}
+
+	/**
+	 * @param UserIdentity $userIdentity
+	 * @return void
+	 */
+	private function refreshUserImpactDataInDeferredUpdate( UserIdentity $userIdentity ): void {
+		DeferredUpdates::addCallableUpdate( function () use ( $userIdentity ) {
+			$impact = $this->userImpactLookup->getExpensiveUserImpact( $userIdentity );
+			if ( $impact ) {
+				$this->userImpactStore->setUserImpact( $impact );
+			}
+		} );
 	}
 }
