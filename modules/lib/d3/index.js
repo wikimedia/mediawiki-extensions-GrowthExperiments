9f10e8e35217857bc974a65566f037c19e9310e6
Kosta Harlan <kharlan@wikimedia.org>
2022-11-09 14:42:53 +0100

    NewImpact: Use d3 subpackages
    Create a custom build of d3 subpackages we need for the Impact module.
    This results in a reduction from 279kB to 115kB.
    
    To generate the minified files, run `npm run rollup`.
    
    Since we're no longer pulling d3.min.js from an npm package dist
    directory, remove the entry in foreign-resources.yaml.
    
    Co-authored-by: Eric Gardner <egardner@wikimedia.org>
    Bug: T321215
    Change-Id: If6f6caf84c33aee4d051cbf5d866b1610ce14b23

---
+++ modules/lib/d3/index.js
@@ -0,0 +1,10 @@
+// Use this file to determine what modules of D3 get included in the bundle.
+// Full list here: https://github.com/d3/d3/blob/main/src/index.js
+export * from "d3-array";
+export * from "d3-interpolate";
+export * from "d3-path";
+export * from "d3-scale";
+export * from "d3-selection";
+export * from "d3-shape";
+export * from "d3-time";
+export * from "d3-time-format";
---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+'use strict';
+const resolve = require( '@rollup/plugin-node-resolve' );
+const terser = require( '@rollup/plugin-terser' );
+
+/**
+ * Config file for generating a custom build of d3, containing only the packages
+ * we need for the Impact module.
+ */
+module.exports = {
+	input: 'modules/lib/d3/index.js',
+	output: {
+		file: 'modules/lib/d3/d3.min.js',
+		format: 'umd',
+		compact: false,
+		name: 'd3'
+	},
+	plugins: [
+		resolve(),
+		terser()
+	],
+	// upstream d3 rollup.config.js has this as well.
+	onwarn( message, warn ) {
+		if ( message.code === 'CIRCULAR_DEPENDENCY' ) {
+			return;
+		}
+		warn( message );
+	}
+};
