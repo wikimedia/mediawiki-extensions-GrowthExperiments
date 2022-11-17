7b12acd8ef9e08c2ee1deb548d2c4a8cacd1cdc9
Sergio Gimeno <sgimeno@wikimedia.org>
2022-11-17 11:18:31 +0100

    vue tests: mock i18n plugin in jest setup
    The mock allows less boilerplating for component
    tests using Vue.prototype.$i18n method. Taken from
    MachineVision extension.
    
    The i18n usage is a bit confusing in the sense that
    it returns an mw.Message object but passing it to a
    Vue template expression works: {{ i18n( 'text-key' ) }}.
    
    We should probably call .text() and prefer passing strings to
    template evaluation functions. See follow-up patches.
    
    Change-Id: I65f46f826463e1a1d7e1fc72fb587f3a11461d16

--- jest.setup.js
+++ jest.setup.js
@@ -1,13 +1,22 @@
+/* global jest:false */
 'use strict';
-/* eslint-disable no-undef */
-// Assign things to "global" here if you want them to be globally available during tests
-global.$ = require( 'jquery' );
+const { config } = require( '@vue/test-utils' );
+// Mock Vue plugins in test suites
+config.global.mocks = {
+	$i18n: ( str ) => str
+};
+
+config.global.directives = {
+	'i18n-html': ( el, binding ) => {
+		el.innerHTML = `${binding.arg} (${binding.value})`;
+	}
+};
 
 function RestMock() {}
 RestMock.prototype.get = jest.fn();
 
 // Mock MW object
-global.mw = {
+const mw = {
 	log: {
 		error: jest.fn(),
 		warn: jest.fn()
@@ -15,6 +24,10 @@ global.mw = {
 	config: {
 		get: jest.fn()
 	},
+	message: jest.fn( ( key ) => ( {
+		text: jest.fn( () => key ),
+		parse: jest.fn()
+	} ) ),
 	user: {
 		getId: jest.fn(),
 		getName: jest.fn(),
@@ -34,3 +47,7 @@ global.mw = {
 	Rest: RestMock
 	// other mw properties as needed...
 };
+
+// Assign things to "global" here if you want them to be globally available during tests
+global.$ = require( 'jquery' );
+global.mw = mw;
