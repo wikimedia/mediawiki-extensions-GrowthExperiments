6ebb6610ee951c31d917d79e1866fc07a96c603a
Sergio Gimeno <sgimeno@wikimedia.org>
2022-12-14 21:00:48 +0100

    Vue components: react to binding updates of v-click-outside directive
    The clickOutside directive was ommiting updates in the binding.value
    which are relevant when the value bound is a reactive property.
    
    CPopover component was passing a close event handler to the clickOutside
    directive regardeless of its state being open or close making
    unnecessary calls for all closed instances. This allows to consistently
    listen to @toggle events. Add also @open and @close events
    for convenience.
    
    Bug: T325041
    Change-Id: I854631a1fe7589e1fb35bb758bf60d51f4d85fbf
    (cherry picked from commit b37777882dc6f67638898cc8ff90d3589be26182)

--- modules/vue-components/directives/click-outside.directive.js
+++ modules/vue-components/directives/click-outside.directive.js
@@ -1,12 +1,22 @@
+const handleClickOutsideOnElement = ( el, event, fn ) => {
+	// here we check that click was outside the el and its children
+	if ( !( el === event.target || el.contains( event.target ) ) ) {
+		// and if it did, call method provided in attribute value
+		fn();
+	}
+};
 module.exports = exports = {
 	beforeMount: ( el, binding ) => {
-		el.clickOutsideEvent = ( event ) => {
-			// here we check that click was outside the el and its children
-			if ( !( el === event.target || el.contains( event.target ) ) ) {
-				// and if it did, call method provided in attribute value
-				binding.value();
-			}
-		};
+		el.clickOutsideEvent = ( event ) => handleClickOutsideOnElement( el, event, binding.value );
+		// set useCapture to true to prevent not reacting to a fired event
+		// which propagation has been (wrongly) stopped.
+		document.addEventListener( 'click', el.clickOutsideEvent, true );
+	},
+	// Allows to react to updates on the binding.value. Necessary to be able
+	// to use computed properties eg: v-click-outside="someReactiveProp ? someFunction : noop"
+	updated: ( el, binding ) => {
+		document.removeEventListener( 'click', el.clickOutsideEvent, true );
+		el.clickOutsideEvent = ( event ) => handleClickOutsideOnElement( el, event, binding.value );
 		// set useCapture to true to prevent not reacting to a fired event
 		// which propagation has been (wrongly) stopped.
 		document.addEventListener( 'click', el.clickOutsideEvent, true );
