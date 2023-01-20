93747697009b27e883b35ba1578bb0ad1f1feacb
Gergő Tisza <tgr.huwiki@gmail.com>
2023-01-19 22:59:14 -0800

    Add Image: Create API module for submitting feedback
    Create a REST API endpoint which wraps around AddImageSubmissionHandler.
    
    Also fix some AddImageSubmissionHandler messages.
    
    Bug: T322309
    Depends-On: I0fec6e145314c81dcb0544aeef9b8a1d139838e5
    Depends-On: I2a9215bf909b83564247ded95ecdb4ead0615150
    Change-Id: Ica712149a890f74a0229c4391086e5111cf674cb

---
+++ includes/Rest/Handler/AddImageFeedbackHandler.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace GrowthExperiments\Rest\Handler;
+
+use ApiMessage;
+use GrowthExperiments\NewcomerTasks\AddImage\AddImageSubmissionHandler;
+use GrowthExperiments\Util;
+use MediaWiki\ParamValidator\TypeDef\TitleDef;
+use MediaWiki\Rest\HttpException;
+use MediaWiki\Rest\LocalizedHttpException;
+use MediaWiki\Rest\SimpleHandler;
+use MediaWiki\Rest\TokenAwareHandlerTrait;
+use MediaWiki\Rest\Validator\JsonBodyValidator;
+use MediaWiki\Rest\Validator\UnsupportedContentTypeBodyValidator;
+use MediaWiki\Rest\Validator\Validator;
+use MediaWiki\Revision\RevisionLookup;
+use MediaWiki\Title\TitleFactory;
+use Status;
+use Wikimedia\Message\MessageValue;
+use Wikimedia\ParamValidator\ParamValidator;
+
+/**
+ * Accept image recommendation feedback. Basically just a wrapper for AddImageSubmissionHandler.
+ */
+class AddImageFeedbackHandler extends SimpleHandler {
+
+	use TokenAwareHandlerTrait;
+
+	private TitleFactory $titleFactory;
+	private RevisionLookup $revisionLookup;
+	private AddImageSubmissionHandler $addImageSubmissionHandler;
+
+	/**
+	 * @param TitleFactory $titleFactory
+	 * @param RevisionLookup $revisionLookup
+	 * @param AddImageSubmissionHandler $addImageSubmissionHandler
+	 */
+	public function __construct(
+		TitleFactory $titleFactory,
+		RevisionLookup $revisionLookup,
+		AddImageSubmissionHandler $addImageSubmissionHandler
+	) {
+		$this->titleFactory = $titleFactory;
+		$this->revisionLookup = $revisionLookup;
+		$this->addImageSubmissionHandler = $addImageSubmissionHandler;
+	}
+
+	public function run() {
+		$user = $this->getAuthority()->getUser();
+		$title = $this->titleFactory->newFromLinkTarget( $this->getValidatedParams()['title'] );
+		$data = $this->getValidatedBody();
+		$editRevId = $data['editRevId'];
+
+		if ( !$user->isRegistered() ) {
+			throw $this->makeException( 'rest-permission-denied-anon', [], 401 );
+		}
+		if ( $data['accepted'] && !$editRevId ) {
+			throw $this->makeException( 'growthexperiments-addimage-feedback-accepted-editrevid' );
+		} elseif ( !$data['accepted'] && $editRevId ) {
+			throw $this->makeException( 'growthexperiments-addimage-feedback-rejected-editrevid' );
+		}
+		if ( $data['accepted'] && ( $data['caption'] ?? null ) === null ) {
+			throw $this->makeException( 'growthexperiments-addimage-feedback-accepted-caption' );
+		}
+
+		$editRev = null;
+		if ( $editRevId !== null ) {
+			$editRev = $this->revisionLookup->getRevisionById( $editRevId );
+			if ( !$editRev ) {
+				throw $this->makeException( 'growthexperiments-addimage-feedback-revid-nonexistent', [ $editRev ] );
+			} elseif ( $editRev->getPageId() !== $title->getArticleID() ) {
+				throw $this->makeException(
+					'growthexperiments-addimage-feedback-invalid-revid-wrong-page',
+					[ $editRevId, $title->getPrefixedText() ]
+				);
+			}
+		}
+		// The handler doesn't use the base revid so make things simple and just fake it.
+		if ( $editRev ) {
+			$baseRev = $this->revisionLookup->getPreviousRevision( $editRev );
+			$baseRevId = $baseRev ? $baseRev->getId() : null;
+		} else {
+			$baseRevId = $title->getLatestRevID();
+		}
+
+		$status = $this->addImageSubmissionHandler->validate( $title->toPageIdentity(), $user, $baseRevId, $data );
+		if ( $status->isGood() ) {
+			$status->merge( $this->addImageSubmissionHandler->handle(
+				$title->toPageIdentity(), $user, $baseRevId, $editRevId, $data
+			), true );
+		}
+		if ( !$status->isGood() ) {
+			Util::logStatus( $status );
+			// There isn't any good way to convert a Message into a MessageValue.
+			$errorKey = ( new ApiMessage( Status::wrap( $status )->getMessage() ) )->getApiCode();
+			throw new HttpException(
+				Status::wrap( $status )->getMessage( false, false, 'en' )->text(),
+				$status->isOK() ? 400 : 500,
+				[ 'errorKey' => $errorKey ]
+			);
+		}
+
+		return [ 'success' => true ] + $status->getValue();
+	}
+
+	/** @inheritDoc */
+	public function getParamSettings() {
+		return [
+			'title' => [
+				self::PARAM_SOURCE => 'path',
+				ParamValidator::PARAM_TYPE => 'title',
+				ParamValidator::PARAM_REQUIRED => true,
+				TitleDef::PARAM_RETURN_OBJECT => true,
+				TitleDef::PARAM_MUST_EXIST => true,
+			],
+		];
+	}
+
+	/** @inheritDoc */
+	public function getBodyValidator( $contentType ) {
+		if ( $contentType === 'application/json' ) {
+			return new JsonBodyValidator(
+				[
+					'editRevId' => [
+						ParamValidator::PARAM_TYPE => 'integer',
+						ParamValidator::PARAM_REQUIRED => false,
+					],
+					'filename' => [
+						ParamValidator::PARAM_TYPE => 'string',
+						ParamValidator::PARAM_REQUIRED => true,
+					],
+					'accepted' => [
+						ParamValidator::PARAM_TYPE => 'boolean',
+						ParamValidator::PARAM_REQUIRED => true,
+					],
+					'reasons' => [
+						ParamValidator::PARAM_TYPE => AddImageSubmissionHandler::REJECTION_REASONS,
+						ParamValidator::PARAM_ISMULTI => true,
+						ParamValidator::PARAM_REQUIRED => false,
+					],
+					'caption' => [
+						ParamValidator::PARAM_TYPE => 'string',
+						ParamValidator::PARAM_REQUIRED => false,
+					],
+				] + $this->getTokenParamDefinition()
+			);
+		}
+		return new UnsupportedContentTypeBodyValidator( $contentType );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function validate( Validator $restValidator ) {
+		parent::validate( $restValidator );
+		$this->validateToken();
+	}
+
+	private function makeException( string $messageKey, array $params = [], int $errorCode = 400 ) {
+		return new LocalizedHttpException( new MessageValue( $messageKey, $params ), $errorCode );
+	}
+
+}
