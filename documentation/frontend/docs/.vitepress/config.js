a063fbcb8b19d6416f4e6636141c26abcee5df9e
Sergio Gimeno <sgimeno@wikimedia.org>
2023-02-10 12:30:49 +0100

    Frontend docs: add build command in the root of the repository
    Add npm run doc command in the root package.json that will install
    the dependencies from the VitePress project in documentation/frontend
    and run the vitepress build command. The generated static files are
    placed in the extension /docs directory. This should allow
    mwext-node14-rundoc-docker job to publish the docs.
    
    Bug: T329034
    Change-Id: I0860e5f8b17b097b7c7f793a20da32cdf2509c35

---
+++ documentation/frontend/docs/.vitepress/config.js
@@ -0,0 +1,13 @@
+export default {
+	lang: 'en-US',
+	title: 'GrowthExperiments',
+	description: 'GrowthExperiments docs and prototypes',
+	themeConfig: {
+		logo: {
+			src: '/logo-Wikimedia.svg', alt: 'Wikimedia'
+		}
+	},
+	// The build output location for the docs site, relative to the VitePress project
+	// root (/documentation/frontend/docs folder if you're running vitepress build docs).
+	outDir: '../../../docs/frontend'
+}
