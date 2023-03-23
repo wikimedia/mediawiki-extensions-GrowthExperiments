304c4f2035c28e7c33f0a0436328acd54866fd27
Sergio Gimeno <sgimeno@wikimedia.org>
2023-03-23 10:57:39 +0100

    Frontend documentation: create a separate demo for the onboarding dialog
    Follow-up: I57dc528edc648cd7fc726ac22e53f3adcfb3d7fa
    Bug: T329037
    Change-Id: I43416430422ac214a43b255e8ad5e2969e9074fb

---
+++ documentation/frontend/component-demos/onboarding-dialog/OnboardingDialogDemo.vue
@@ -0,0 +1,60 @@
+<template>
+	<div>
+		<cdx-button @click="open = true">
+			Show dialog
+		</cdx-button>
+		<!-- eslint-disable vue/no-v-model-argument -->
+		<onboarding-dialog
+			v-model:open="open"
+			:total-steps="2"
+			:initial-step="0"
+		>
+			<template #title>
+				<h5>A slotted <i>Onboarding ddialog</i> header</h5>
+			</template>
+			<template #headerbtntext>
+				<b>Skip all</b>
+			</template>
+			<template #body="{ currentIndex }">
+				<onboarding-step
+					:current-index="currentIndex"
+					:index="1"
+				>
+					<template #main>
+						Step 1
+					</template>
+				</onboarding-step>
+				<onboarding-step
+					:current-index="currentIndex"
+					:index="2"
+				>
+					<template #main>
+						Step 2
+					</template>
+				</onboarding-step>
+			</template>
+		</onboarding-dialog>
+	</div>
+</template>
+
+<script>
+import { CdxButton } from '@wikimedia/codex';
+import { ref } from 'vue';
+import OnboardingDialog from '../../components/OnboardingDialog.vue';
+import OnboardingStep from '../../components/OnboardingStep.vue';
+export default {
+	name: 'OnboardingDialogDemo',
+
+	components: {
+		CdxButton,
+		OnboardingDialog,
+		OnboardingStep
+	},
+	setup() {
+		const open = ref( false );
+		return {
+			open
+		};
+	}
+};
+</script>
---
+++ documentation/frontend/docs/demos/onboarding-dialog.md
@@ -0,0 +1,11 @@
+<script setup>
+import '../../node_modules/@wikimedia/codex/dist/codex.style.css';
+import OnboardingDialogDemo from '../../component-demos/onboarding-dialog/OnboardingDialogDemo.vue'
+</script>
+
+Onboarding Dialog Vue prototype
+===============================
+
+::: raw
+<OnboardingDialogDemo />
+:::
