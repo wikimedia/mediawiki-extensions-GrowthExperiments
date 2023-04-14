16184f77bf8badf139340be52586460581b62dfc
Sergio Gimeno <sgimeno@wikimedia.org>
2023-04-14 12:53:08 +0200

    Frontend documentation: add content to OnboardingDialog component demo page
    Add description, usage and examples to onboarding-dialog.md
    Add dialog example without steps
    
    Follow-up: I57dc528edc648cd7fc726ac22e53f3adcfb3d7fa
    Bug: T333346
    Change-Id: I8b4d7c39a0f1f28dc247212b5abf5efbece6bb16

---
+++ documentation/frontend/component-demos/onboarding-simple-dialog/OnboardingSimpleDialogDemo.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="ext-growthExperiments-OnboardingSimpleDialogDemo">
+		<cdx-button @click="open = true">
+			Show dialog
+		</cdx-button>
+		<!-- eslint-disable vue/no-v-model-argument -->
+		<onboarding-dialog
+			v-model:open="open"
+			:total-steps="1"
+			:initial-step="1"
+			@close="onDialogClose"
+		>
+			<template #title>
+				A simple <i>Onboarding dialog</i>
+			</template>
+			<template #headerbtntext>
+				Close
+			</template>
+			<h5>This is a simple dialog without steps.</h5>
+			<p>This dialog doesn't include paginator, header button, and checkbox.</p>
+		</onboarding-dialog>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import { CdxButton } from '@wikimedia/codex';
+import OnboardingDialog from '../../components/OnboardingDialog.vue';
+
+export default {
+	name: 'OnboardingSimpleDialogDemo',
+
+	components: {
+		CdxButton,
+		OnboardingDialog
+	},
+
+	setup() {
+		const open = ref( false );
+
+		function onDialogClose( result ) {
+			// eslint-disable-next-line no-console
+			console.log( 'Dialog closed', result );
+		}
+
+		return {
+			open,
+			onDialogClose
+		};
+	}
+};
+</script>
+
+<style lang="less">
+.ext-growthExperiments-OnboardingSimpleDialogDemo {
+	padding-top: 2em;
+}
+</style>
