6fea756f3204b0573cd5aebe82073d899587afa9
Gergő Tisza <tgr.huwiki@gmail.com>
2023-05-31 19:18:10 +0200

    Section images: Implement image placeholder
    Implement the "placeholder" for section images where we show
    an icon to indicate where the image will be inserted, before
    user approval of the image (when insertion actually happens).
    
    There are two ways to achieve this: make the placeholder a
    subclass of MWImageNode (either by having RecommendedImageNode
    act as both placeholder and actual image, depending on some
    flag that's managed as a node attribute; or by having separate
    image and placeholder node types, both MWImageNode subclasses)
    or make it a custom node that's not derived from VE types.
    
    This patch does the latter, on the grounds that the placeholder
    will have a fairly different DOM from the actual image, only
    needs very simple data model + contenteditable behavior
    (although most behavior is blocked by readonly mode anyway so
    that doesn't matter much), and the effort needed to simulate
    the image node's layout and positioning isn't large.
    This means having to reimplement skin/device specific image
    formatting, but not having to deal with unintended consequences
    of having a DOM node that uses image thumbnail classes but
    doesn't exactly have the same structure and expected design as
    actual images.
    
    Other changes:
    * Fix specificity of image node max-width rule, which was
      getting overruled on mobile.
    
    Fixing the scrolling / transitions is left for another patch.
    
    Bug: T335209
    Change-Id: Ie400067723eb7fad94dd26a058b1361aaa1210ee

---
+++ modules/ext.growthExperiments.StructuredTask/addsectionimage/ceRecommendedImagePlaceholderNode.js
@@ -0,0 +1,34 @@
+/**
+ * @class mw.libs.ge.ce.RecommendedImagePlaceholderNode
+ * @extends ve.ce.LeafNode
+ * @constructor
+ */
+function CERecommendedImagePlaceholderNode() {
+	CERecommendedImagePlaceholderNode.super.apply( this, arguments );
+	this.$element
+		.css( {
+			// width is the sum of:
+			// - 2x8px padding on desktop
+			// - the image
+			width: this.model.getAttribute( 'width' ) + ( OO.ui.isMobile() ? 0 : 16 ),
+			// height is the sum of:
+			// - 2x7px padding on desktop / 2x9.6px padding on mobile
+			// - header (desktop only): 32 px icon height + 8px bottom margin
+			// - the image
+			// - caption: 4px/6.5px (desktop/mobile) top margin + 2x2 px border + 7+27px padding
+			//   + content height (which we are guessing here as 60px).
+			height: this.model.getAttribute( 'height' ) + ( OO.ui.isMobile() ? 130 : 156 )
+		} )
+		.addClass( 'ge-section-image-placeholder' )
+		// a somewhat hacky way of adding the 'image' OOUI icon (progressive variant) as background
+		.addClass( 'oo-ui-image-progressive oo-ui-icon-image' );
+}
+OO.inheritClass( CERecommendedImagePlaceholderNode, ve.ce.LeafNode );
+
+/** @inheritDoc */
+CERecommendedImagePlaceholderNode.static.name = 'mwGeRecommendedImagePlaceholder';
+
+/** @inheritDoc */
+CERecommendedImagePlaceholderNode.static.tagName = 'div';
+
+module.exports = CERecommendedImagePlaceholderNode;
