c6f4d8996ebbb5af952dcc1d0fc1c4320d44a8c1
Sergio Gimeno <sgimeno@wikimedia.org>
2023-06-29 17:22:29 +0200

    User impact: set module unactive until user edits in mainspace
    Why:
     - Remove potential counfounding change that has impact
    on the observed activation decrease
    
    What:
     - Change the module unactivated rule
    
    Note: non-mainspace edits are still counted and part
    of the total edit shown. However their pages are not
    listed under articles list.
    
    Bug: T338640
    Change-Id: Ie3a46499342f6c494eb0d8da294809bab80922c8

--- ServiceWiring.php
+++ ServiceWiring.php
@@ -1106,6 +1106,7 @@ return [
 		MediaWikiServices $services
 	): UserDatabaseHelper {
 		return new UserDatabaseHelper(
+			$services->getUserFactory(),
 			$services->getDBLoadBalancer()->getConnection( DB_REPLICA )
 		);
 	},
