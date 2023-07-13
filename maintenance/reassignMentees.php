af5d540399ace98ec179660de89fe2c409d18371
Martin Urbanec <martin.urbanec@wikimedia.cz>
2023-07-13 14:59:23 +0200

    Add reassignMentees.php maintenance script
    Why:
    Before the structured mentor list was deployed,
    mentors removed themselves by only editing the wikitext
    list of mentors. This caused many former mentors to have
    mentees assigned (and ocasionally questions asked).
    
    This was previously handled by a standalone Python
    script, which was fine for one-off requests. Now,
    we want to reassign mentees belonging to the ex-mentors
    on all projects, and it is tedious to run the Python
    script everywhere.
    
    What:
    Introduce `reassignMentees.php` maintenance script,
    which is able to both find ex-mentors with assigned
    mentees and reassign mentees belonging to them
    to different mentors.
    
    For pre-runs and individual cases in the future,
    the possibility to reassign mentees assigned
    to a given mentor is also provided.
    
    This is somewhat based on https://phabricator.wikimedia.org/P37040.
    
    Bug: T330071
    Change-Id: Ie7545e14c48cee498d4bbc5f16cf1a324196ad80

---
+++ maintenance/reassignMentees.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace GrowthExperiments\Maintenance;
+
+use GrowthExperiments\GrowthExperimentsServices;
+use GrowthExperiments\Mentorship\Provider\MentorProvider;
+use GrowthExperiments\Mentorship\ReassignMenteesFactory;
+use GrowthExperiments\Mentorship\Store\MentorStore;
+use Maintenance;
+use MediaWiki\MediaWikiServices;
+use MediaWiki\User\UserIdentity;
+use MediaWiki\User\UserIdentityLookup;
+use RequestContext;
+use Wikimedia\Rdbms\IReadableDatabase;
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+require_once "$IP/maintenance/Maintenance.php";
+
+class ReassignMentees extends Maintenance {
+
+	private IReadableDatabase $growthDbr;
+	private UserIdentityLookup $userIdentityLookup;
+	private MentorProvider $mentorProvider;
+	private ReassignMenteesFactory $reassignMenteesFactory;
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->requireExtension( 'GrowthExperiments' );
+		$this->addDescription(
+			'Reassign mentees assigned to a particular user (who is not in the mentor list)'
+		);
+		$this->addOption(
+			'performer',
+			'Who should the reassignments be attributed to?',
+			true,
+			true
+		);
+		$this->addOption(
+			'all',
+			'Reassign mentees assigned to all non-mentor users'
+		);
+		$this->addOption(
+			'mentor',
+			'Username of the mentor to do the reassigning for',
+			false,
+			true
+		);
+	}
+
+	private function init() {
+		$services = MediaWikiServices::getInstance();
+		$growthServices = GrowthExperimentsServices::wrap( $services );
+		$growthLb = $growthServices->getLoadBalancer();
+
+		$this->growthDbr = $growthLb->getConnection( DB_REPLICA );
+		$this->userIdentityLookup = $services->getUserIdentityLookup();
+		$this->mentorProvider = $growthServices->getMentorProvider();
+		$this->reassignMenteesFactory = $growthServices->getReassignMenteesFactory();
+	}
+
+	/**
+	 * @return UserIdentity[]
+	 */
+	private function getAllUnofficialMentors(): array {
+		$officialMentors = iterator_to_array( $this->userIdentityLookup->newSelectQueryBuilder()
+			->whereUserNames( $this->mentorProvider->getMentors() )
+			->fetchUserIdentities() );
+		$officialMentorIds = array_map( static function ( UserIdentity $user ) {
+			return $user->getId();
+		}, $officialMentors );
+
+		$unofficialMentorIds = $this->growthDbr->newSelectQueryBuilder()
+			->select( 'gemm_mentor_id' )
+			->distinct()
+			->from( 'growthexperiments_mentor_mentee' )
+			->where( [
+				'gemm_mentor_role' => MentorStore::ROLE_PRIMARY,
+				'gemm_mentor_id NOT IN (' . $this->growthDbr->makeList( $officialMentorIds ) . ')'
+			] )
+			->fetchFieldValues();
+
+		if ( $unofficialMentorIds === [] ) {
+			return [];
+		}
+
+		return iterator_to_array( $this->userIdentityLookup->newSelectQueryBuilder()
+			->whereUserIds( $unofficialMentorIds )
+			->fetchUserIdentities() );
+	}
+
+	/**
+	 * @return UserIdentity[]
+	 */
+	private function getMentorsToProcess(): array {
+		$mentors = [];
+		if ( !$this->hasOption( 'all' ) && !$this->hasOption( 'mentor' ) ) {
+			$this->fatalError( 'ERROR: Either --all or --mentor needs to be provided.' );
+		} elseif ( $this->hasOption( 'all' ) && $this->hasOption( 'mentor' ) ) {
+			$this->fatalError( 'ERROR: --all and --mentor together do not make sense.' );
+		} elseif ( $this->hasOption( 'mentor' ) ) {
+			$mentor = $this->userIdentityLookup->getUserIdentityByName( $this->getOption( 'mentor' ) );
+			if ( !$mentor ) {
+				$this->fatalError( 'ERROR: User not found.' );
+			}
+			$mentors[] = $mentor;
+		} elseif ( $this->hasOption( 'all' ) ) {
+			$mentors = $this->getAllUnofficialMentors();
+		}
+		return $mentors;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function execute() {
+		$this->init();
+
+		$performerName = $this->getOption( 'performer' );
+		$performer = $this->userIdentityLookup->getUserIdentityByName( $performerName );
+		if ( !$performer ) {
+			$this->fatalError( 'ERROR: User "' . $performerName . '" does not exist.' );
+		}
+
+		$mentors = $this->getMentorsToProcess();
+		foreach ( $mentors as $mentor ) {
+			if ( $this->mentorProvider->isMentor( $mentor ) ) {
+				$this->error(
+					"ERROR: User \"{$mentor->getName()}\" is currently enrolled as a mentor. " .
+					'Remove them before the reassignment.'
+				);
+				continue;
+			}
+
+			$this->reassignMenteesFactory->newReassignMentees(
+				$performer,
+				$mentor,
+				RequestContext::getMain()
+			)->doReassignMentees(
+				'growthexperiments-quit-mentorship-reassign-mentees-log-message',
+				$mentor->getName()
+			);
+		}
+
+		$this->output( 'Done!' . PHP_EOL );
+	}
+}
+
+$maintClass = ReassignMentees::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
