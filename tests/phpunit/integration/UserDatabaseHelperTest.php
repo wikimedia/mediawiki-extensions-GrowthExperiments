5fe292d89d5d3c04af8f59996e9e2cf5760b1ae0
Martin Urbanec <martin.urbanec@wikimedia.cz>
2023-07-17 17:57:31 +0200

    [tests] Add UserDatabaseHelperTest
    Regression test for the bug reported as T341994.
    
    Bug: T341994
    Change-Id: Ib8c2ac801b76b3aea22515458aa8bcfc00362bf0

---
+++ tests/phpunit/integration/UserDatabaseHelperTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace GrowthExperiments\Tests;
+
+use GrowthExperiments\GrowthExperimentsServices;
+use MediaWikiIntegrationTestCase;
+
+/**
+ * @group medium
+ * @group Database
+ * @coversDefaultClass \GrowthExperiments\UserDatabaseHelper
+ */
+class UserDatabaseHelperTest extends MediaWikiIntegrationTestCase {
+
+	/**
+	 * @covers ::hasMainspaceEdits
+	 */
+	public function testHasMainspaceEditsWithNoEdits() {
+		$this->assertSame(
+			false,
+			GrowthExperimentsServices::wrap( $this->getServiceContainer() )
+				->getUserDatabaseHelper()
+				->hasMainspaceEdits( $this->getTestUser()->getUserIdentity() )
+		);
+	}
+
+	/**
+	 * @covers ::hasMainspaceEdits
+	 * @dataProvider provideHasMainspaceEdits
+	 * @param bool|null $expectedResult
+	 * @param int $namespaceId
+	 * @param bool $overLimit
+	 */
+	public function testHasMainspaceEditsWithEdits(
+		?bool $expectedResult,
+		int $namespaceId,
+		bool $overLimit
+	) {
+		$user = $this->getMutableTestUser()->getUser();
+		$limit = 5;
+		$editsToSave = $overLimit ? $limit : $limit - 1;
+		for ( $i = 0; $i < $editsToSave; $i++ ) {
+			$this->editPage(
+				'Sandbox',
+				"test $i",
+				'',
+				$namespaceId,
+				$user
+			);
+		}
+
+		$this->assertSame(
+			$expectedResult,
+			GrowthExperimentsServices::wrap( $this->getServiceContainer() )
+				->getUserDatabaseHelper()
+				->hasMainspaceEdits( $user, $limit )
+		);
+	}
+
+	public static function provideHasMainspaceEdits() {
+		return [
+			'NS_MAIN edits, over limit' => [ true, NS_MAIN, true ],
+			'NS_MAIN edits, under limit' => [ true, NS_MAIN, false ],
+			'NS_PROJECT edits, over limit' => [ null, NS_PROJECT, true ],
+			'NS_PROJECT edits, under limit' => [ false, NS_PROJECT, false ],
+		];
+	}
+}
