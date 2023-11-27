00fca6e826aec99b77ec7060b774b4f6e7ece6b6
Martin Urbanec <martin.urbanec@wikimedia.cz>
2023-11-27 12:21:10 +0100

    tests: Add basic coverage for SpecialEditGrowthConfig
    Ensure the special page executes fine in read only mode.
    This is by no means a perfect coverage, but it would prevent
    bugs like T351603 when Special:EditGrowthConfig was just completely
    inaccessible.
    
    Give it at least a very basic test coverage, to prevent issues
    like T351603 from happening.
    
    Bug: T351603
    Change-Id: I13e6bb15626498381b39a4c6eddf95a7f732c141

---
+++ tests/phpunit/integration/Specials/SpecialEditGrowthConfigTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace GrowthExperiments\Tests;
+
+use GrowthExperiments\Config\GrowthExperimentsMultiConfig;
+use GrowthExperiments\GrowthExperimentsServices;
+use GrowthExperiments\Specials\SpecialEditGrowthConfig;
+use LogicException;
+use SpecialPageTestBase;
+
+/**
+ * @coversDefaultClass \GrowthExperiments\Specials\SpecialEditGrowthConfig
+ * @group Database
+ */
+class SpecialEditGrowthConfigTest extends SpecialPageTestBase {
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function newSpecialPage() {
+		$geServices = GrowthExperimentsServices::wrap( $this->getServiceContainer() );
+		$wikiConfig = $geServices->getGrowthWikiConfig();
+		if ( !$wikiConfig instanceof GrowthExperimentsMultiConfig ) {
+			throw new LogicException(
+				'GrowthExperimentsMultiConfig service is expected to be an instance of GrowthExperimentsMultiConfig'
+			);
+		}
+		return new SpecialEditGrowthConfig(
+			$this->getServiceContainer()->getTitleFactory(),
+			$this->getServiceContainer()->getRevisionLookup(),
+			$this->getServiceContainer()->getPageProps(),
+			$this->getServiceContainer()->getDBLoadBalancer(),
+			$this->getServiceContainer()->getReadOnlyMode(),
+			$geServices->getWikiPageConfigLoader(),
+			$geServices->getWikiPageConfigWriterFactory(),
+			$wikiConfig
+		);
+	}
+
+	public static function provideConfigOverrides() {
+		$overrides = [
+			'default config' => [],
+			'levelling up disabled' => [ 'GELevelingUpFeaturesEnabled' => false ],
+		];
+		foreach ( $overrides as $overrideName => $override ) {
+			yield $overrideName => [ $override ];
+		}
+	}
+
+	/**
+	 * @covers ::execute
+	 * @dataProvider provideConfigOverrides
+	 */
+	public function testExecutes( array $configOverrides ) {
+		$this->setMwGlobals( $configOverrides );
+
+		/** @var string $html */
+		list( $html, ) = $this->executeSpecialPage();
+		$this->assertNotEmpty( $html );
+	}
+}
