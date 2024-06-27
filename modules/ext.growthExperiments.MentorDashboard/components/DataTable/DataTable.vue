b94d45f65d8cc889d3535065f3390349363b7bcd
Jon Robson <jdlrobson@gmail.com>
2024-06-27 13:43:14 -0700

    Fixes colors of data table sort icons and header in dark mode
    Bug: T366376
    Change-Id: I96f6f505edd21f580552ec0ba77ae4398802a90b

--- modules/ext.growthExperiments.MentorDashboard/components/DataTable/DataTable.vue
+++ modules/ext.growthExperiments.MentorDashboard/components/DataTable/DataTable.vue
@@ -195,6 +195,18 @@ module.exports = exports = {
 			&--descending {
 				background-image: url( ../../../../../../resources/src/jquery.tablesorter.styles/images/sort_down.svg );
 			}
+
+			// stylelint-disable-next-line selector-class-pattern
+			html.skin-theme-clientpref-night & {
+				filter: invert( 1 );
+			}
+
+			@media ( prefers-color-scheme: dark ) {
+				// stylelint-disable-next-line selector-class-pattern
+				html.skin-theme-clientpref-os & {
+					filter: invert( 1 );
+				}
+			}
 		}
 
 		thead {
@@ -208,7 +220,7 @@ module.exports = exports = {
 			}
 
 			tr {
-				background-color: #f8f9fa;
+				background-color: @background-color-neutral-subtle;
 				height: 47px;
 			}
 		}
