1ac6e5fbde1e050a1664359e21949c7f086c347b
Michael Große <mgrosse@wikimedia.org>
2024-09-06 11:20:40 +0200

    tests: Add Cypress test for Special:Impact
    Starting with a simple test to assert the Special:Impact does indeed
    show a main-namespace edit of a new user.
    
    Based on other Cypress configurations in the MediaWiki extensions
    EntitySchema, Cite, and WikibaseLexeme.
    
    Bug: T373321
    Change-Id: I96590539aa9999a7f5c3b870f2c903ea9db687d4

---
+++ cypress/support/MwApiPlugin.ts
@@ -0,0 +1,87 @@
+/**
+ * Helper methods for generic MediaWiki API functionality separate from the Cypress browser context
+ *
+ * This file is intended to be extracted into a separate npm package,
+ * so that it can be used across extensions.
+ */
+
+// needed for api-testing library, see api-testing/lib/config.js
+process.env.REST_BASE_URL = process.env.MW_SERVER + process.env.MW_SCRIPT_PATH + '/';
+
+import { clientFactory, utils } from 'api-testing';
+
+const state = {
+	users: {},
+};
+
+function debugLog( ...args: unknown[] ): void {
+	if ( !process.env.MW_DEBUG ) {
+		return;
+	}
+	console.log( ...args );
+}
+
+module.exports = {
+	mwApiCommands( cypressConfig ) {
+		// TODO: replace the `any` once the api-testing library type definitions available
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		async function root(): Promise<any> {
+			if ( state.users.root ) {
+				return state.users.root;
+			}
+
+			debugLog( 'Getting new root user client' );
+			const rootClient = clientFactory.getActionClient( null );
+			await rootClient.login(
+				cypressConfig.mediawikiAdminUsername,
+				cypressConfig.mediawikiAdminPassword,
+			);
+			await rootClient.loadTokens( [ 'createaccount', 'userrights', 'csrf' ] );
+
+			const rightsToken = await rootClient.token( 'userrights' );
+			if ( rightsToken === '+\\' ) {
+				throw new Error( 'Failed to get the root user tokens.' );
+			}
+
+			state.users.root = rootClient;
+			return rootClient;
+		}
+
+		return {
+			async 'MwApi:CreateUser'( { usernamePrefix } ) {
+				const rootUser = await root();
+				const username = utils.title( usernamePrefix + '-' );
+				const password = utils.uniq();
+
+				debugLog( 'Creating account for', username );
+				await rootUser.createAccount( { username: username, password: password } );
+
+				const userClient = clientFactory.getActionClient( null );
+				await userClient.login(
+					username,
+					password,
+				);
+
+				state.users[ username ] = userClient;
+
+				return Promise.resolve( { username, password } );
+			},
+			async 'MwApi:Edit'( { username, title, text, summary } ) {
+				const userClient = state.users[ username ];
+				if ( !userClient ) {
+					throw new Error( 'User not found.' );
+				}
+				const token = await userClient.token( 'csrf' );
+
+				const editResult = await userClient.action( 'edit', { title, text, summary, token }, true );
+
+				if ( editResult.edit.result !== 'Success' ) {
+					console.error( 'Edit failed', editResult );
+					return Promise.reject( new Error( 'edit failed: ' + editResult.edit.result ) );
+				}
+
+				return Promise.resolve( null );
+			},
+		};
+	},
+};
