b93d10c46b3b6e13da824407af47c6957621fa55
Michael Große <mgrosse@wikimedia.org>
2024-12-02 13:00:44 +0100

    test(cypress): fail if there is an error in the console
    We need to keep track of all the spies we create in a test, in case we
    navigate to multiple pages during a test.
    
    The `trackSurfacing` method is not available right away when a page is
    loaded, but only after resource loader is fully initialized. Thus we
    keep waiting until it is available. Sometimes that means just waiting
    for the already queued async actions (thus the `0` initial timeout), but
    sometimes a bit more time is needed and thus we are retrying in 100ms
    increments.
    Overall, it might make sense to move that logic into a dedicated file or
    to upstream it somewhere, given that it is largely independent of
    GrowthExperiments.
    
    The changes to the LocalSettings are necessary because there actually
    was an error in the Impact-test coming from the suggested edits module,
    complaining that the TaskSuggesterFactory was not correctly set up. This
    now adds a stub for that to this change. Follow-up changes will fill
    that factory with proper testing for migrating the existing add-a-link
    and add-a-image and template-based task changes.
    
    Bug: T377198
    Change-Id: If651480d6a928d710c00a810c809a37bfc80b2da

--- cypress/support/setupFixtures/GrowthExperiments.LocalSettings.php
+++ cypress/support/setupFixtures/GrowthExperiments.LocalSettings.php
@@ -1,6 +1,20 @@
 <?php
 
+use GrowthExperiments\NewcomerTasks\TaskSuggester\StaticTaskSuggesterFactory;
+use GrowthExperiments\NewcomerTasks\TaskSuggester\TaskSuggesterFactory;
+use MediaWiki\MediaWikiServices;
+
 $wgGEUseCommunityConfigurationExtension = true;
 $wgGENewcomerTasksLinkRecommendationsEnabled = true;
 $wgGELinkRecommendationsFrontendEnabled = true;
 $wgGESurfacingStructuredTasksEnabled = true;
+
+$wgHooks['MediaWikiServices'][] = static function ( MediaWikiServices $services ) {
+	# Mock the task suggester to specify what article(s) will be suggested.
+	$services->redefineService(
+		'GrowthExperimentsTaskSuggesterFactory',
+		static function () use ( $services ): TaskSuggesterFactory {
+			return new StaticTaskSuggesterFactory( [], $services->getTitleFactory() );
+		}
+	);
+};
