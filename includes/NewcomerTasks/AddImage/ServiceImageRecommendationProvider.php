wikimedia/mediawiki-extensions-GrowthExperiments

commit ee8695f59f94aa976b10b134cbe9e2163507efa0

fix(tracking): TimingMetric:observe records milliseconds
It turns out, see T355837#10438043 and following comments, that it now
actually matters in what unit the timings are being provided. If it is
the wrong unit, the recorded data is essentially useless.

We also cannot use `copyToStatsdAt`, because that would essentially
multiply our existing metrics by 1000, see T359248#9994136 for this
happening.

It is sadly necessary to inject the dependency into
LocalSearchTaskSuggester because otherwise its unittest fails.

Bug: T383208
Change-Id: I8e80de1c99f476acde290c7d442d05f5c62c369d
Depends-On: I0c56cc8153318dc79b94a8bc390b7dfabcf8ca38

includes/NewcomerTasks/AddImage/ServiceImageRecommendationProvider.php
@@ -9,6 +9,7 @@ use MediaWiki\Api\ApiRawMessage;
 use MediaWiki\Language\RawMessage;
 use MediaWiki\Linker\LinkTarget;
 use MediaWiki\Logger\LoggerFactory;
+use MediaWiki\MediaWikiServices;
 use MediaWiki\Page\ProperPageIdentity;
 use MediaWiki\Title\Title;
 use MediaWiki\Title\TitleFactory;
@@ -99,11 +100,19 @@ class ServiceImageRecommendationProvider implements ImageRecommendationProvider 
 
 		$startTime = microtime( true );
 		$status = $request->execute();
+		$getRequestTimeInSeconds = microtime( true ) - $startTime;
 
 		$timing = $this->statsFactory->getTiming( 'image_recommendation_provider_seconds' );
 		$timing->setLabel( 'action', 'get' )
-			->copyToStatsdAt( "timing.growthExperiments.imageRecommendationProvider.get" )
-			->observe( microtime( true ) - $startTime );
+			->observeSeconds( $getRequestTimeInSeconds );
+
+		// Stay backward compatible with the legacy Graphite-based dashboard
+		// feeding on this data.
+		// TODO: remove after switching to Prometheus-based dashboards
+		MediaWikiServices::getInstance()->getStatsdDataFactory()->timing(
+			'timing.growthExperiments.imageRecommendationProvider.get',
+			$getRequestTimeInSeconds
+		);
 
 		if ( !$status->isOK() && $request->getStatus() < 400 ) {
 			return $status;
@@ -142,12 +151,19 @@ class ServiceImageRecommendationProvider implements ImageRecommendationProvider 
 			$this->maxSuggestionsToProcess
 		);
 
+		$processingTimeInSeconds = microtime( true ) - $startTime;
 		$timing
 			->setLabel( 'action', 'process_api_response_data' )
-			->copyToStatsdAt(
-				"timing.growthExperiments.imageRecommendationProvider.processApiResponseData"
-			)
-			->observe( microtime( true ) - $startTime );
+			->observeSeconds( $processingTimeInSeconds );
+
+		// Stay backward compatible with the legacy Graphite-based dashboard
+		// feeding on this data.
+		// TODO: remove after switching to Prometheus-based dashboards
+		$services = MediaWikiServices::getInstance();
+		$services->getStatsdDataFactory()->timing(
+			'timing.growthExperiments.imageRecommendationProvider.processApiResponseData',
+			$getRequestTimeInSeconds
+		);
 
 		return $responseData;
 	}

ServiceWiring.php
@@ -889,7 +889,8 @@ return [
 				$growthServices->getNewcomerTasksUserOptionsLookup(),
 				$services->getSearchEngineFactory(),
 				$services->getLinkBatchFactory(),
-				$services->getStatsFactory()
+				$services->getStatsFactory(),
+				$services->getStatsdDataFactory()
 			);
 			$taskSuggesterFactory = new DecoratingTaskSuggesterFactory(
 				$taskSuggesterFactory,

includes/Api/ApiQueryLinkRecommendations.php
@@ -158,7 +158,7 @@ class ApiQueryLinkRecommendations extends ApiQueryBase {
 		$this->statsFactory->withComponent( 'GrowthExperiments' )
 			->getTiming( 'surfacing_link_recommendation_api_processing_candidate_seconds' )
 			->setLabel( 'wiki', WikiMap::getCurrentWikiId() )
-			->observe( microtime( true ) - $processingCandidateStartTimeSeconds );
+			->observeSeconds( microtime( true ) - $processingCandidateStartTimeSeconds );
 		return $linkRecommendation;
 	}
 

includes/NewcomerTasks/TaskSuggester/LocalSearchTaskSuggester.php
@@ -18,6 +18,7 @@ use MediaWiki\User\UserIdentity;
 use SearchEngine;
 use SearchEngineFactory;
 use StatusValue;
+use Wikimedia\Stats\IBufferingStatsdDataFactory;
 use Wikimedia\Stats\StatsFactory;
 
 /**
@@ -29,6 +30,7 @@ class LocalSearchTaskSuggester extends SearchTaskSuggester {
 	private $searchEngineFactory;
 
 	private StatsFactory $statsFactory;
+	private IBufferingStatsdDataFactory $statsdDataFactory;
 
 	/**
 	 * @param TaskTypeHandlerRegistry $taskTypeHandlerRegistry
@@ -39,6 +41,7 @@ class LocalSearchTaskSuggester extends SearchTaskSuggester {
 	 * @param TaskType[] $taskTypes
 	 * @param Topic[] $topics
 	 * @param StatsFactory $statsFactory
+	 * @param IBufferingStatsdDataFactory $statsdDataFactory
 	 */
 	public function __construct(
 		TaskTypeHandlerRegistry $taskTypeHandlerRegistry,
@@ -48,12 +51,14 @@ class LocalSearchTaskSuggester extends SearchTaskSuggester {
 		LinkBatchFactory $linkBatchFactory,
 		array $taskTypes,
 		array $topics,
-		StatsFactory $statsFactory
+		StatsFactory $statsFactory,
+		IBufferingStatsdDataFactory $statsdDataFactory
 	) {
 		parent::__construct( $taskTypeHandlerRegistry, $searchStrategy, $newcomerTasksUserOptionsLookup,
 			$linkBatchFactory, $taskTypes, $topics );
 		$this->searchEngineFactory = $searchEngineFactory;
 		$this->statsFactory = $statsFactory->withComponent( 'GrowthExperiments' );
+		$this->statsdDataFactory = $statsdDataFactory;
 	}
 
 	/** @inheritDoc */
@@ -66,11 +71,18 @@ class LocalSearchTaskSuggester extends SearchTaskSuggester {
 	) {
 		$start = microtime( true );
 		$suggest = parent::suggest( $user, $taskSetFilters, $limit, $offset, $options );
+		$suggestTimeInSeconds = microtime( true ) - $start;
 		$this->statsFactory->getTiming( 'search_task_suggester_seconds' )
 			->setLabel( 'task_suggester', 'local' )
 			->setLabel( 'action', 'suggest' )
-			->copyToStatsdAt( "timing.growthExperiments.SearchTaskSuggester.suggest" )
-			->observe( microtime( true ) - $start );
+			->observeSeconds( $suggestTimeInSeconds );
+
+		// Stay backward compatible with the legacy Graphite-based dashboard
+		// feeding on this data.
+		// TODO: remove after switching to Prometheus-based dashboards
+		$this->statsdDataFactory->timing(
+			'timing.growthExperiments.SearchTaskSuggester.suggest', $suggestTimeInSeconds
+		);
 		return $suggest;
 	}
 
@@ -78,11 +90,18 @@ class LocalSearchTaskSuggester extends SearchTaskSuggester {
 	public function filter( UserIdentity $user, TaskSet $taskSet ) {
 		$start = microtime( true );
 		$filter = parent::filter( $user, $taskSet );
+		$filterTimeInSeconds = microtime( true ) - $start;
 		$this->statsFactory->getTiming( 'search_task_suggester_seconds' )
 			->setLabel( 'task_suggester', 'local' )
 			->setLabel( 'action', 'filter' )
-			->copyToStatsdAt( "timing.growthExperiments.SearchTaskSuggester.filter" )
-			->observe( microtime( true ) - $start );
+			->observeSeconds( $filterTimeInSeconds );
+
+		// Stay backward compatible with the legacy Graphite-based dashboard
+		// feeding on this data.
+		// TODO: remove after switching to Prometheus-based dashboards
+		$this->statsdDataFactory->timing(
+			'timing.growthExperiments.SearchTaskSuggester.filter', $filterTimeInSeconds
+		);
 		return $filter;
 	}
 
@@ -141,19 +160,25 @@ class LocalSearchTaskSuggester extends SearchTaskSuggester {
 			$query->setDebugUrl( SpecialPage::getTitleFor( 'Search' )
 				->getFullURL( $params, false, PROTO_CANONICAL ) );
 		}
-		$elapsed = microtime( true ) - $start;
+		$elapsedInSeconds = microtime( true ) - $start;
 		$this->logger->debug( 'LocalSearchTaskSuggester query', [
 			'query' => $query->getQueryString(),
 			'sort' => $query->getSort(),
 			'limit' => $limit,
 			'success' => $matches instanceof ISearchResultSet,
-			'elapsedTime' => $elapsed
+			'elapsedTime' => $elapsedInSeconds
 		] );
 		$this->statsFactory->getTiming( 'search_task_suggester_seconds' )
 			->setLabel( 'task_suggester', 'local' )
 			->setLabel( 'action', 'search' )
-			->copyToStatsdAt( "timing.growthExperiments.LocalSearchTaskSuggester.search" )
-			->observe( $elapsed );
+			->observeSeconds( $elapsedInSeconds );
+
+		// Stay backward compatible with the legacy Graphite-based dashboard
+		// feeding on this data.
+		// TODO: remove after switching to Prometheus-based dashboards
+		$this->statsdDataFactory->timing(
+			'timing.growthExperiments.LocalSearchTaskSuggester.search', $elapsedInSeconds
+		);
 
 		return $matches;
 	}

includes/NewcomerTasks/TaskSuggester/LocalSearchTaskSuggesterFactory.php
@@ -9,6 +9,7 @@ use GrowthExperiments\NewcomerTasks\TaskType\TaskTypeHandlerRegistry;
 use MediaWiki\Cache\LinkBatchFactory;
 use SearchEngineFactory;
 use StatusValue;
+use Wikimedia\Stats\IBufferingStatsdDataFactory;
 use Wikimedia\Stats\StatsFactory;
 
 /**
@@ -20,6 +21,7 @@ class LocalSearchTaskSuggesterFactory extends SearchTaskSuggesterFactory {
 	private $searchEngineFactory;
 
 	private StatsFactory $statsFactory;
+	private IBufferingStatsdDataFactory $statsdDataFactory;
 
 	/**
 	 * @param TaskTypeHandlerRegistry $taskTypeHandlerRegistry
@@ -29,6 +31,7 @@ class LocalSearchTaskSuggesterFactory extends SearchTaskSuggesterFactory {
 	 * @param SearchEngineFactory $searchEngineFactory
 	 * @param LinkBatchFactory $linkBatchFactory
 	 * @param StatsFactory $statsFactory
+	 * @param IBufferingStatsdDataFactory $statsdDataFactory
 	 */
 	public function __construct(
 		TaskTypeHandlerRegistry $taskTypeHandlerRegistry,
@@ -37,7 +40,8 @@ class LocalSearchTaskSuggesterFactory extends SearchTaskSuggesterFactory {
 		NewcomerTasksUserOptionsLookup $newcomerTasksUserOptionsLookup,
 		SearchEngineFactory $searchEngineFactory,
 		LinkBatchFactory $linkBatchFactory,
-		StatsFactory $statsFactory
+		StatsFactory $statsFactory,
+		IBufferingStatsdDataFactory $statsdDataFactory
 	) {
 		parent::__construct(
 			$taskTypeHandlerRegistry,
@@ -48,6 +52,7 @@ class LocalSearchTaskSuggesterFactory extends SearchTaskSuggesterFactory {
 		);
 		$this->searchEngineFactory = $searchEngineFactory;
 		$this->statsFactory = $statsFactory;
+		$this->statsdDataFactory = $statsdDataFactory;
 	}
 
 	/**
@@ -72,7 +77,8 @@ class LocalSearchTaskSuggesterFactory extends SearchTaskSuggesterFactory {
 			$this->linkBatchFactory,
 			$taskTypes,
 			$topics,
-			$this->statsFactory
+			$this->statsFactory,
+			$this->statsdDataFactory
 		);
 		$suggester->setLogger( $this->logger );
 		return $suggester;

includes/Rest/Handler/UserImpactHandler.php
@@ -10,6 +10,7 @@ use GrowthExperiments\UserImpact\UserImpactLookup;
 use GrowthExperiments\UserImpact\UserImpactStore;
 use JobQueueGroup;
 use MediaWiki\Deferred\DeferredUpdates;
+use MediaWiki\MediaWikiServices;
 use MediaWiki\ParamValidator\TypeDef\UserDef;
 use MediaWiki\Rest\HttpException;
 use MediaWiki\Rest\SimpleHandler;
@@ -73,10 +74,17 @@ class UserImpactHandler extends SimpleHandler {
 		}
 		$formattedJsonData = $this->userImpactFormatter->format( $userImpact, $pageviewsUrlDisplayLanguageCode );
 
+		$runTimeInSeconds = microtime( true ) - $start;
 		$this->statsFactory->withComponent( 'GrowthExperiments' )
 			->getTiming( 'user_impact_handler_run_seconds' )
-			->copyToStatsdAt( 'timing.growthExperiments.UserImpactHandler.run' )
-			->observe( microtime( true ) - $start );
+			->observeSeconds( $runTimeInSeconds );
+
+		// Stay backward compatible with the legacy Graphite-based dashboard
+		// feeding on this data.
+		// TODO: remove after switching to Prometheus-based dashboards
+		MediaWikiServices::getInstance()->getStatsdDataFactory()->timing(
+			'timing.growthExperiments.UserImpactHandler.run', $runTimeInSeconds
+		);
 
 		return $formattedJsonData;
 	}

includes/Specials/SpecialHomepage.php
@@ -20,6 +20,7 @@ use MediaWiki\Config\ConfigException;
 use MediaWiki\Deferred\DeferredUpdates;
 use MediaWiki\Html\Html;
 use MediaWiki\Logger\LoggerFactory;
+use MediaWiki\MediaWikiServices;
 use MediaWiki\Registration\ExtensionRegistry;
 use MediaWiki\SpecialPage\SpecialPage;
 use MediaWiki\Title\TitleFactory;
@@ -138,11 +139,20 @@ class SpecialHomepage extends SpecialPage {
 			$this->experimentUserManager->getVariant( $this->getUser() )
 		);
 		$platform = ( $this->isMobile ? 'mobile' : 'desktop' );
+		$overallSsrTimeInSeconds = microtime( true ) - $startTime;
 		$this->statsFactory->withComponent( 'GrowthExperiments' )
 			->getTiming( 'special_homepage_server_side_render_seconds' )
 			->setLabel( 'platform', $platform )
-			->copyToStatsdAt( "timing.growthExperiments.specialHomepage.serverSideRender." . $platform )
-			->observe( microtime( true ) - $startTime );
+			->observeSeconds( $overallSsrTimeInSeconds );
+
+		// Stay backward compatible with the legacy Graphite-based dashboard
+		// feeding on this data.
+		// TODO: remove after switching to Prometheus-based dashboards
+		$services = MediaWikiServices::getInstance();
+		$services->getStatsdDataFactory()->timing(
+			'timing.growthExperiments.specialHomepage.serverSideRender.' . $platform,
+			$overallSsrTimeInSeconds
+		);
 
 		if ( $loggingEnabled &&
 			 ExtensionRegistry::getInstance()->isLoaded( 'EventLogging' ) &&
@@ -296,17 +306,27 @@ class SpecialHomepage extends SpecialPage {
 		}
 	}
 
-	private function recordModuleRenderingTime( string $moduleName, string $mode, float $timeToRecord ): void {
+	private function recordModuleRenderingTime( string $moduleName, string $mode, float $timeToRecordInSeconds ): void {
 		$wiki = WikiMap::getCurrentWikiId();
 		$this->statsFactory->withComponent( 'GrowthExperiments' )
-			->getTiming( 'special_homepage_ssr_milliseconds' )
+			->getTiming( 'special_homepage_ssr_per_module_seconds' )
 			->setLabel( 'wiki', $wiki )
 			->setLabel( 'module', $moduleName )
 			->setLabel( 'mode', $mode )
-			->copyToStatsdAt( implode( '.', [
-				$wiki, 'timing.growthExperiments.specialHomepage.ssr', $moduleName, $mode
-			] ) )
-			->observe( $timeToRecord );
+			->observeSeconds( $timeToRecordInSeconds );
+
+		// Stay backward compatible with the legacy Graphite-based dashboard
+		// feeding on this data.
+		// TODO: remove after switching to Prometheus-based dashboards
+		$services = MediaWikiServices::getInstance();
+		$services->getPerDbNameStatsdDataFactory()->timing(
+			implode( '.', [
+				'timing.growthExperiments.specialHomepage.ssr',
+				$moduleName,
+				$mode,
+			] ),
+			$timeToRecordInSeconds
+		);
 	}
 
 	/**

includes/UserImpact/ComputedUserImpactLookup.php
@@ -11,6 +11,7 @@ use MediaWiki\Config\ServiceOptions;
 use MediaWiki\Extension\PageViewInfo\PageViewService;
 use MediaWiki\Extension\Thanks\ThanksQueryHelper;
 use MediaWiki\MainConfigNames;
+use MediaWiki\MediaWikiServices;
 use MediaWiki\Registration\ExtensionRegistry;
 use MediaWiki\Revision\RevisionRecord;
 use MediaWiki\Status\Status;
@@ -195,11 +196,20 @@ class ComputedUserImpactLookup implements UserImpactLookup {
 			ComputeEditingStreaks::getLongestEditingStreak( $editData->getEditCountByDay() ),
 			$this->userEditTracker->getUserEditCount( $user )
 		);
+		$userImpactLookupDurationInSeconds = microtime( true ) - $start;
 		$this->statsFactory->withComponent( 'GrowthExperiments' )
 			->getTiming( 'computed_user_impact_lookup_expensive_seconds' )
-			->copyToStatsdAt(
-				'timing.growthExperiments.ComputedUserImpactLookup.getExpensiveUserImpact' )
-			->observe( microtime( true ) - $start );
+			->observeSeconds( $userImpactLookupDurationInSeconds );
+
+		// Stay backward compatible with the legacy Graphite-based dashboard
+		// feeding on this data.
+		// TODO: remove after switching to Prometheus-based dashboards
+		$services = MediaWikiServices::getInstance();
+		$services->getStatsdDataFactory()->timing(
+			'timing.growthExperiments.ComputedUserImpactLookup.getExpensiveUserImpact',
+			$userImpactLookupDurationInSeconds
+		);
+
 		return $expensiveUserImpact;
 	}
 

commit 48a98f2cfd682cf654bc7d3e14632d43db7db641

fix: track correct data in legacy Graphite query
This is a bug that was introduced in ee8695f. In that commit, the statslib call was
tracking the correct variable, but the legacy Graphite/statsd one
was copied with the metric name from line 114. This change fixes that so
that the statslib call and the legacy statsd call are both tracking the
same data.

Bug: T383208
Change-Id: Ic62260994422d260ca6f74a97652a738d802391d

includes/NewcomerTasks/AddImage/ServiceImageRecommendationProvider.php
@@ -162,7 +162,7 @@ class ServiceImageRecommendationProvider implements ImageRecommendationProvider 
 		$services = MediaWikiServices::getInstance();
 		$services->getStatsdDataFactory()->timing(
 			'timing.growthExperiments.imageRecommendationProvider.processApiResponseData',
-			$getRequestTimeInSeconds
+			$processingTimeInSeconds
 		);
 
 		return $responseData;