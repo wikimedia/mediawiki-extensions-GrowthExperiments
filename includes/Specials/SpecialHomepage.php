62207d39cf5741e4002ae8eb1dfc570b5ebd7cc7
Cyndy <csimiyu@wikimedia.org>
2025-02-06 12:56:22 +0300

    Community Updates: End pilot experiment
    Bug: T385338
    Change-Id: I9aebdb6476113b29716cee780489f30f3795c25a

--- includes/Specials/SpecialHomepage.php
+++ includes/Specials/SpecialHomepage.php
@@ -13,7 +13,6 @@ use GrowthExperiments\HomepageModules\SuggestedEdits;
 use GrowthExperiments\Mentorship\MentorManager;
 use GrowthExperiments\TourHooks;
 use GrowthExperiments\Util;
-use GrowthExperiments\VariantHooks;
 use InvalidArgumentException;
 use MediaWiki\Config\Config;
 use MediaWiki\Config\ConfigException;
@@ -213,10 +212,7 @@ class SpecialHomepage extends SpecialPage {
 			),
 			'suggested-edits' => SuggestedEdits::isEnabled( $this->getConfig() ),
 			'community-updates' => Util::useCommunityConfiguration() &&
-				$this->getConfig()->get( 'GECommunityUpdatesEnabled' ) &&
-				$this->experimentUserManager->isUserInVariant( $this->getUser(), [
-					VariantHooks::VARIANT_COMMUNITY_UPDATES_MODULE
-				] ),
+				$this->getConfig()->get( 'GECommunityUpdatesEnabled' ),
 			'impact' => true,
 			'mentorship' => $this->wikiConfig->get( 'GEMentorshipEnabled' ) &&
 				$mentorshipState === MentorManager::MENTORSHIP_ENABLED,
