4247b78bf8924a610a1ded4ee19658401903b5a2
Martin Urbanec <martin.urbanec@wikimedia.cz>
2025-02-17 19:29:31 +0100

    testing(api): Remove importDump
    None of the API tests appear to depend on this.
    Let's remove the importing, and readd it in the future
    if it becomes necessary.
    
    Bug: T384423
    Change-Id: I4389796163ea6c7371964ccf1550b735c1549972

--- tests/api-testing/fixtures.cjs
+++ tests/api-testing/fixtures.cjs
@@ -1,18 +1,9 @@
 'use strict';
-const path = require( 'path' );
-const childProcess = require( 'child_process' );
-const ip = path.resolve( __dirname + '/../../../../' );
 const LocalSettingsSetup = require( __dirname + '/LocalSettingsSetup.cjs' );
 
 exports.mochaGlobalSetup = async function () {
 	await LocalSettingsSetup.overrideLocalSettings();
 	await LocalSettingsSetup.restartPhpFpmService();
-	// Import the test articles and their suggestions
-	childProcess.spawnSync(
-		'php',
-		[ 'maintenance/run.php', 'importDump', path.resolve( __dirname + '/SuggestedEditsContent.xml' ) ],
-		{ cwd: ip }
-	);
 };
 
 exports.mochaGlobalTeardown = async function () {
