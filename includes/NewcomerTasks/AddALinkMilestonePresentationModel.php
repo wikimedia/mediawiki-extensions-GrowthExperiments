8d0529177f0c025f60fc4cd60703d5878a65e1b7
Cyndy <csimiyu@wikimedia.org>
2025-06-13 11:13:00 +0300

    Send notification when Add a Link edit threshold is reached
    Notification alerts newcomers when they reach the
    community-configured edit threshold for Add a Link
    tasks.
    The notification informs users that Add a Link
    tasks are no longer available and congratulate them
    on their achievement.
    
    The notification targets users who meet these criteria:
    - Have more than X edits (community-configured via T395383)
    - Completed at least one Add a Link task in the past
    - Still have Add a Link selected in their task preferences
    
    Bug: T393771
    Change-Id: I80ac6651aeb84d49715c49a539df413c2abc2167

---
+++ includes/NewcomerTasks/AddALinkMilestonePresentationModel.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace GrowthExperiments\NewcomerTasks;
+
+use MediaWiki\Extension\Notifications\Formatters\EchoEventPresentationModel;
+use MediaWiki\Message\Message;
+use MediaWiki\SpecialPage\SpecialPage;
+
+class AddALinkMilestonePresentationModel extends EchoEventPresentationModel {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getIconType() {
+		return 'growthexperiments-addalink-milestone';
+	}
+
+	public function getHeaderMessageKey(): string {
+		return 'growthexperiments-addalink-milestone-notification-header';
+	}
+
+	public function getHeaderMessage(): Message {
+		$threshold = $this->event->getExtraParam( 'threshold' );
+		$viewsCount = $this->event->getExtraParam( 'views-count', 0 );
+		$msg = $this->msg( $this->getHeaderMessageKey() );
+		$msg->numParams( $threshold )
+			->params( $this->getViewingUserForGender() )
+			->numParams( $viewsCount );
+
+		return $msg;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getPrimaryLink() {
+		return false;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getSecondaryLinks() {
+		return [ [
+			'url' => $this->getSpecialHomepageUrl( 'get-started-primary-link-' . $this->getDistributionType() ),
+			'label' => $this->msg(
+				'growthexperiments-levelingup-getstarted-notification-call-to-action-label'
+			)->text()
+		] ];
+	}
+
+	/**
+	 * Get the URL for Special:Homepage with suggested edits fragment
+	 *
+	 * @param string $source Analytics source parameter
+	 * @return string Full URL with fragment
+	 */
+	private function getSpecialHomepageUrl( string $source ): string {
+		$title = SpecialPage::getTitleFor( 'Homepage' );
+		$url = $title->getFullURL( [ 'source' => $source ] );
+
+		return $url . '#/homepage/suggested-edits';
+	}
+}
---
+++ tests/phpunit/integration/NewcomerTasks/MediawikiEventSubscribers/NewcomerMilestoneIngressTest.php
@@ -0,0 +1,149 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace GrowthExperiments\Tests\Integration;
+
+use GrowthExperiments\ErrorException;
+use MediaWiki\CommentStore\CommentStoreComment;
+use MediaWiki\Content\WikitextContent;
+use MediaWiki\Extension\CommunityConfiguration\CommunityConfigurationServices;
+use MediaWiki\Extension\Notifications\DbFactory;
+use MediaWiki\Extension\Notifications\Mapper\NotificationMapper;
+use MediaWiki\Revision\SlotRecord;
+use MediaWiki\User\User;
+use MediaWikiIntegrationTestCase;
+use Wikimedia\Rdbms\Platform\ISQLPlatform;
+
+/**
+ * @group Database
+ * @group Echo
+ * @covers \GrowthExperiments\NewcomerTasks\MediaWikiEventIngress\NewcomerMilestoneIngress
+ */
+class NewcomerMilestoneIngressTest extends MediaWikiIntegrationTestCase {
+
+	protected function setUp(): void {
+		parent::setUp();
+		$this->markTestSkippedIfExtensionNotLoaded( 'Echo' );
+		$this->overrideConfigValues( [
+			'GENewcomerTasksLinkRecommendationsEnabled' => true,
+			'GENewcomerTasksStarterDifficultyEnabled' => true
+		] );
+	}
+
+	/**
+	 * @return array[]
+	 */
+	public static function provideNotificationScenarios(): array {
+		return [
+			'notification sent when threshold reached' => [
+				'userEditCount' => 4,
+				'maxEditsThreshold' => 5,
+				'addLinkEditCount' => 1,
+				'expectedNotificationCount' => 1,
+			],
+			'no notification when no add-link edits' => [
+				'userEditCount' => 4,
+				'maxEditsThreshold' => 5,
+				'addLinkEditCount' => 0,
+				'expectedNotificationCount' => 0,
+			],
+			'no notification when threshold not reached' => [
+				'userEditCount' => 3,
+				'maxEditsThreshold' => 5,
+				'addLinkEditCount' => 1,
+				'expectedNotificationCount' => 0,
+			],
+			'no notification when threshold not set' => [
+				'userEditCount' => 4,
+				'maxEditsThreshold' => null,
+				'addLinkEditCount' => 1,
+				'expectedNotificationCount' => 0,
+			],
+		];
+	}
+
+	/**
+	 * @dataProvider provideNotificationScenarios
+	 */
+	public function testMilestoneNotification(
+		int $userEditCount,
+		?int $maxEditsThreshold,
+		int $addLinkEditCount,
+		int $expectedNotificationCount
+	): void {
+		$this->clearEchoData();
+		$this->setMaxEditsTaskIsAvailableInConfig( $maxEditsThreshold ? (string)$maxEditsThreshold : 'no' );
+		$user = $this->getMutableTestUser()->getUser();
+		$this->setUserEditCount( $user, $userEditCount );
+		if ( $addLinkEditCount > 0 ) {
+			// Simulate an addlink edit
+			$page = $this->getExistingTestPage();
+			$pageUpdater = $page->newPageUpdater( $user );
+			$pageUpdater->setContent( SlotRecord::MAIN, new WikitextContent( 'newcomer edit' ) );
+			$pageUpdater->addTags( [ 'newcomer task add link' ] );
+			$pageUpdater->saveRevision( CommentStoreComment::newUnsavedComment( 'edit by newcomer' ) );
+		} else {
+			// Simulate a random edit
+			$this->editPage( 'Test', 'Test-edit-1', 'Test', NS_MAIN, $user );
+		}
+
+		$notificationMapper = new NotificationMapper();
+		$notifications = $notificationMapper->fetchByUser(
+			$user,
+			10,
+			null,
+			[ 'newcomer-milestone-reached' ]
+		);
+
+		$this->assertCount(
+			$expectedNotificationCount,
+			$notifications,
+			"Expected $expectedNotificationCount notification(s) for milestone threshold"
+		);
+	}
+
+	/**
+	 * @throws ErrorException
+	 */
+	private function setMaxEditsTaskIsAvailableInConfig( string $selectedEnumValue = 'no' ): void {
+		$communityConfigServices = CommunityConfigurationServices::wrap( $this->getServiceContainer() );
+		$suggestedEditsProvider = $communityConfigServices
+			->getConfigurationProviderFactory()->newProvider( 'GrowthSuggestedEdits' );
+		$status = $suggestedEditsProvider->loadValidConfiguration();
+		$config = null;
+		if ( $status->isOK() ) {
+			$config = $status->getValue();
+			$config->{'link_recommendation'}->{'maximumEditsTaskIsAvailable'} = $selectedEnumValue;
+		}
+		$status = $suggestedEditsProvider->storeValidConfiguration(
+			$config, $this->getTestUser( [ 'interface-admin' ] )->getUser()
+		);
+		if ( !$status->isOK() ) {
+			throw new ErrorException( $status );
+		}
+	}
+
+	private function setUserEditCount( User $user, int $editCount ): void {
+		$this->getDb()->newUpdateQueryBuilder()
+			->update( 'user' )
+			->set( [ 'user_editcount' => $editCount ] )
+			->where( [ 'user_id' => $user->getId() ] )
+			->caller( __METHOD__ )
+			->execute();
+	}
+
+	private function clearEchoData(): void {
+		$db = DbFactory::newFromDefault()->getEchoDb( DB_PRIMARY );
+		$db->newDeleteQueryBuilder()
+			->deleteFrom( 'echo_event' )
+			->where( ISQLPlatform::ALL_ROWS )
+			->caller( __METHOD__ )
+			->execute();
+		$db->newDeleteQueryBuilder()
+			->deleteFrom( 'echo_notification' )
+			->where( ISQLPlatform::ALL_ROWS )
+			->caller( __METHOD__ )
+			->execute();
+	}
+}
