957a596edafb4749054612135058a4a723b6a104
Michael Große <mgrosse@wikimedia.org>
2025-11-05 11:38:14 +0100

    refactor(ReviseTone): extract revise tone onboarding component
    This should probably not live in the main structured task component but
    a level deeper, even though it will probably take us some time before we
    migrate the other components.
    
    Change-Id: I85bd239028a2a88e1b79a18c046bb36ade55ca8c

---
+++ modules/ext.growthExperiments.StructuredTask.PreEdit/revisetone/ReviseToneOnboarding.vue
@@ -0,0 +1,102 @@
+<template>
+	<onboarding-dialog
+		v-model:open="open"
+		v-model:is-checked="isChecked"
+		class="ext-growthExperiments-ReviseToneOnboarding"
+		:initial-step="1"
+		:total-steps="totalSteps"
+		:stepper-label="stepperLabelText"
+		:close-button-text="$i18n( 'growthexperiments-revisetone-onboarding-dialog-skip-label' ).text()"
+		:start-button-text="$i18n( 'growthexperiments-structuredtask-onboarding-dialog-get-started-button' ).text()"
+		:checkbox-label="$i18n( 'growthexperiments-structuredtask-onboarding-dialog-dismiss-checkbox' ).text()"
+		@update:current-step="onStepChange"
+		@close="reset"
+	>
+		<template #title>
+			<div
+				v-i18n-html:growthexperiments-revisetone-onboarding-dialog-title-label="[ userName ]"
+				class="ext-growthExperiments-ReviseToneOnboarding__title"
+			>
+			</div>
+		</template>
+
+		<template
+			v-for="( quiz, index ) in quizData"
+			#[`step${index+1}`]
+			:key="`step${index + 1}`"
+		>
+			<quiz-game
+				v-model:result="quizResults[index]"
+				:data="quiz"
+				@update:result="( newVal ) => quizResults[index] = newVal"
+			></quiz-game>
+		</template>
+	</onboarding-dialog>
+</template>
+
+<script>
+const OnboardingDialog = require( '../common/OnboardingDialog.vue' );
+const QuizGame = require( './QuizGame.vue' );
+// FIXME data should come from static variable, config?
+const quizData = require( './__mocks__/quizsData.json' );
+const { computed, defineComponent, inject, ref, reactive } = require( 'vue' );
+
+// @vue/component
+module.exports = defineComponent( {
+	name: 'ReviseToneOnboarding',
+	components: { OnboardingDialog, QuizGame },
+	props: {
+		prefName: {
+			type: [ String, null ],
+			default: null,
+		},
+	},
+	setup( props ) {
+		const i18n = inject( 'i18n' );
+		const Api = inject( 'mw.Api' );
+		const mwUser = inject( 'mw.user' );
+		const mwHook = inject( 'mw.hook' );
+		const open = ref( true );
+		const isChecked = ref( false );
+		const quizResults = reactive( [] );
+		const totalSteps = quizData.length;
+		const currentStep = ref( 1 );
+		const stepperLabelText = computed(
+			() => i18n( 'growthexperiments-structuredtask-onboarding-dialog-progress', currentStep.value, totalSteps ).text(),
+		);
+		const onStepChange = ( newVal ) => {
+			currentStep.value = newVal;
+		};
+		const saveDismissed = () => new Api().saveOption( props.prefName, isChecked.value ? '1' : '0' );
+		const reset = () => {
+			// Reset each game result in the rare case the dialog is re-opened
+			quizResults.splice( 0 );
+			// Store the checkbox value
+			saveDismissed();
+			// Fire the structured task
+			mwHook( 'growthExperiments.structuredTask.onboardingCompleted' ).fire();
+		};
+		return {
+			isChecked,
+			onStepChange,
+			open,
+			quizData,
+			quizResults,
+			reset,
+			stepperLabelText,
+			totalSteps,
+			userName: mwUser.getName(),
+		};
+	},
+} );
+
+</script>
+
+<style lang="less">
+.ext-growthExperiments-ReviseToneOnboarding {
+	// Force vertical stacking of elements in the title label
+	&__title > * {
+		display: block;
+	}
+}
+</style>
