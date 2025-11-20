08cf3f9148962acbad2137b17a76ea84ba1d4337
Sergio Gimeno <sgimeno@wikimedia.org>
2025-11-20 12:50:48 +0100

    ReviseToneExperimentEditIngress: add debug statements
    Try to understand why edit events are not received/sent in beta
    cluster
    
    Bug: T405177
    Change-Id: Iaad8145c8fd4a26a120758a41b395f793e6107f0

--- includes/NewcomerTasks/MediaWikiEventIngress/ReviseToneExperimentEditIngress.php
+++ includes/NewcomerTasks/MediaWikiEventIngress/ReviseToneExperimentEditIngress.php
@@ -6,31 +6,50 @@ use MediaWiki\DomainEvent\DomainEventIngress;
 use MediaWiki\Extension\MetricsPlatform\XLab\ExperimentManager;
 use MediaWiki\Page\Event\PageLatestRevisionChangedEvent;
 use MediaWiki\Page\Event\PageLatestRevisionChangedListener;
+use Psr\Log\LoggerInterface;
 
 class ReviseToneExperimentEditIngress extends DomainEventIngress implements PageLatestRevisionChangedListener {
 
 	public function __construct(
-		private readonly ?ExperimentManager $experimentManager = null
+		private readonly LoggerInterface $logger,
+		private readonly ?ExperimentManager $experimentManager = null,
 	) {
 	}
 
 	public function handlePageLatestRevisionChangedEvent( PageLatestRevisionChangedEvent $event ): void {
+		$revId = $event->getLatestRevisionAfter()->getId();
+		$this->logger->debug( 'ReviseToneExperimentEditIngress, processing {rev_id}: start', [
+			'rev_id' => $revId,
+		] );
 		// Is MetricsPlatform loaded?
 		if ( !$this->experimentManager ) {
 			return;
 		}
+		$this->logger->debug( 'ReviseToneExperimentEditIngress, processing {rev_id}: MetricsPlatform loaded', [
+			'rev_id' => $revId,
+		] );
 
 		$editResult = $event->getEditResult();
 		if ( $editResult == null || $editResult->isNullEdit() || $event->isRevert() ) {
 			return;
 		}
-
+		$this->logger->debug( 'ReviseToneExperimentEditIngress, processing {rev_id}: get experiment info', [
+			'rev_id' => $revId,
+		] );
 		$experiment = $this->experimentManager->getExperiment(
 			ExperimentXLabManager::REVISE_TONE_EXPERIMENT
 		);
+		$this->logger->debug( 'ReviseToneExperimentEditIngress, processing {rev_id}: experiment configuration', [
+			'rev_id' => $revId,
+			...$experiment->getExperimentConfig(),
+		] );
 		$experiment->send( 'edit_saved', [
 			// TODO add additional props, maybe nothing is needed but to check that all contextual attributes are
 			// properly added by the Metrics client
+		] );
+		$this->logger->debug( 'ReviseToneExperimentEditIngress, processing {rev_id}: end', [
+			'rev_id' => $revId,
+			...$experiment->getExperimentConfig(),
 		] );
 	}
 }
