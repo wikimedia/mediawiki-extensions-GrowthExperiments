f88c1d53f680f612716ff7c529966d76f1096881
SomeRandomDeveloper <thisisnotmyname275@gmail.com>
2025-11-26 22:49:32 +0100

    SECURITY: Escape system messages used in edit summaries
    Bug: T411144
    Change-Id: Iff01940a163ed87ec52f3a64ba6b2dbfa2759df3

--- includes/HomepageHooks.php
+++ includes/HomepageHooks.php
@@ -1431,7 +1431,7 @@ class HomepageHooks implements
 			$messageParamsStr = $messageParts[ 1 ] ?? '';
 			$comment = wfMessage( $messageKey )
 				->numParams( ...explode( '|', $messageParamsStr ) )
-				->parse();
+				->escaped();
 		}
 	}
 
--- includes/Mentorship/Hooks/MentorHooks.php
+++ includes/Mentorship/Hooks/MentorHooks.php
@@ -282,7 +282,7 @@ class MentorHooks implements
 			'growthexperiments-mentorship-enrollasmentor-summary',
 		];
 		if ( in_array( $auto, $noParamMessageKeys ) ) {
-			$comment = wfMessage( $auto )->text();
+			$comment = wfMessage( $auto )->escaped();
 		}
 
 		$mentorChangeMessageKeys = [
@@ -306,7 +306,7 @@ class MentorHooks implements
 			$comment = wfMessage( $messageKey )
 				->params( ...explode( '|', $messageParts[1] ) )
 				->inContentLanguage()
-				->parse();
+				->escaped();
 		}
 	}
 
