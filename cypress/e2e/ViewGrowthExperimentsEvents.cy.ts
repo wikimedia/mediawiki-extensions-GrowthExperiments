61be82567c6635acd894d32832c889581afec28b
Marcin Szwarc <mszwarc@wikimedia.org>
2025-12-08 12:40:28 +0100

    Don't use fixed #ooui-php-... ids in ViewGrowthExperimentsEvents test
    Why:
    - OOUI ids are generated automatically and, while deterministic, may
      change when something else in the form changes.
    - An unrelated patch, Ifb593042fd, caused the test to fail, because
      the widgets' ids were reassigned.
    
    What:
    - Change the selectors to `select[name=...]`, so that they are stable.
    
    Change-Id: I1c9a0d68d712c5074684594b6bfca7d0687939b3

--- cypress/e2e/ViewGrowthExperimentsEvents.cy.ts
+++ cypress/e2e/ViewGrowthExperimentsEvents.cy.ts
@@ -2,9 +2,9 @@ describe( 'Special:Log', () => {
 	it( 'Shows the GrowthExperiments event Mentor assignment changes', () => {
 		cy.loginAsAdmin();
 		cy.visit( 'index.php?title=Special:Log' );
-		cy.get( '#ooui-php-1' ).select( 'GrowthExperiments log', { force: true } );
+		cy.get( 'select[name=type]' ).select( 'GrowthExperiments log', { force: true } );
 		cy.contains( 'button', 'Show' ).click();
 		cy.get( '.oo-ui-labelElement-label' ).contains( 'Type of GrowthExperiments event:' );
-		cy.get( '#ooui-php-9' ).select( 'Mentor assignment changes', { force: true } );
+		cy.get( 'select[name=subtype]' ).select( 'Mentor assignment changes', { force: true } );
 	} );
 } );
