8e3011659b8bf29116bb1118add2abd60d794c97
Bartosz Dziewoński <dziewonski@fastmail.fm>
2026-01-12 22:44:42 +0100

    tests: Fix ViewGrowthExperimentsEvents for upcoming MediaWiki changes
    Needed-By: Ie3112a4b83532f500851ec17e51b35b2f9907c4a
    Change-Id: I3a851f0e624d5f59c274c68fcb809f42e62c5ef3

--- cypress/e2e/ViewGrowthExperimentsEvents.cy.ts
+++ cypress/e2e/ViewGrowthExperimentsEvents.cy.ts
@@ -5,6 +5,9 @@ describe( 'Special:Log', () => {
 		cy.get( 'select[name=type]' ).select( 'GrowthExperiments log', { force: true } );
 		cy.contains( 'button', 'Show' ).click();
 		cy.get( '.oo-ui-labelElement-label' ).contains( 'Type of GrowthExperiments event:' );
-		cy.get( 'select[name=subtype]' ).select( 'Mentor assignment changes', { force: true } );
+		cy.get(
+			// Two options for compatibility before and after Ie3112a4b83 (T320871)
+			'#mw-input-subtype select[name=subtype], #mw-log-action-filter-growthexperiments select[name=subtype]',
+		).select( 'Mentor assignment changes', { force: true } );
 	} );
 } );
