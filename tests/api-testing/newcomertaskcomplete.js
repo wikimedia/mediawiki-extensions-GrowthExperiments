f706505ce507face3757c34bb197a6de1be24f28
Umherirrender <umherirrender_de.wp@web.de>
2026-01-17 11:23:35 +0100

    tests: Split REST path in api-testing to make pass
    The path normalization with api-testing@1.7.2 adds always a trailing
    slash (1c019d46cadb664b21506f59b33d49fd2bd811df), which results in 404
    as the RestRoutes is defined without trailing slash for this endpoint as
    the endpoints uses query parameter and not path parameters
    
    Change-Id: Ie80dd558260e8fc7b3e0603d57baf4500888f474

--- tests/api-testing/newcomertaskcomplete.js
+++ tests/api-testing/newcomertaskcomplete.js
@@ -4,30 +4,30 @@ const { assert, REST } = require( 'api-testing' );
 
 describe( 'POST /growthexperiments/v0/newcomertask/complete', () => {
 
-	const client = new REST( 'rest.php/growthexperiments/v0/newcomertask/complete' );
+	const client = new REST( 'rest.php/growthexperiments/v0/' );
 
 	it( 'the endpoint is not accessible to anonymous users', async () => {
-		const { body: sourceBody } = await client.post( '?taskTypeId=foo&revId=123' );
+		const { body: sourceBody } = await client.post( 'newcomertask/complete?taskTypeId=foo&revId=123' );
 		assert.strictEqual( sourceBody.httpCode, 403 );
 		assert.strictEqual( sourceBody.message, 'You must be logged-in' );
 	} );
 
 	it( 'revId param is required', async () => {
-		const { body: sourceBody } = await client.post( '?taskTypeId=foo' );
+		const { body: sourceBody } = await client.post( 'newcomertask/complete?taskTypeId=foo' );
 		assert.strictEqual( sourceBody.httpCode, 400 );
 		assert.strictEqual( sourceBody.name, 'revId' );
 		assert.strictEqual( sourceBody.failureCode, 'missingparam' );
 	} );
 
 	it( 'taskTypeId param is required', async () => {
-		const { body: sourceBody } = await client.post( '?revId=foo' );
+		const { body: sourceBody } = await client.post( 'newcomertask/complete?revId=foo' );
 		assert.strictEqual( sourceBody.httpCode, 400 );
 		assert.strictEqual( sourceBody.name, 'taskTypeId' );
 		assert.strictEqual( sourceBody.failureCode, 'missingparam' );
 	} );
 
 	it( 'revId must be an integer', async () => {
-		const { body: sourceBody } = await client.post( '?revId=foo&taskTypeId=bar' );
+		const { body: sourceBody } = await client.post( 'newcomertask/complete?revId=foo&taskTypeId=bar' );
 		assert.strictEqual( sourceBody.httpCode, 400 );
 		assert.strictEqual( sourceBody.name, 'revId' );
 		assert.strictEqual( sourceBody.failureCode, 'badinteger' );
