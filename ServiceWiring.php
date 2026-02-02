31b88c67c9b39df50a9214d0484109c5beec997e
Sergio Gimeno <sgimeno@wikimedia.org>
2026-02-02 16:09:08 +0100

    DatabaseUserImpactStore: log attempts to save zero pageviews values
    We're observing user impacts with a high number of 0s as pageviews
    values for dailyArticleViews computed in ComputedUserImpactLookup::getPageViewData.
    This is unexpected as there's a filtering applied precisely to
    prevent unnecessary data causing big blobs.
    
    Attempt to log information that helps identify where/when are we storing
    these.
    
    Bug: T414080
    Change-Id: I30e86191bfc70abea106abd677dd7a66c16f4839
    (cherry picked from commit 8e94f77c806995dce12a87a8cacf6b3dd8064986)

--- ServiceWiring.php
+++ ServiceWiring.php
@@ -1205,7 +1205,10 @@ return [
 		MediaWikiServices $services
 	): UserImpactLookup {
 		$growthServices = GrowthExperimentsServices::wrap( $services );
-		return new DatabaseUserImpactStore( $growthServices->getLoadBalancer() );
+		return new DatabaseUserImpactStore(
+			$growthServices->getLoadBalancer(),
+			$growthServices->getLogger(),
+		);
 	},
 
 	'GrowthExperimentsUserImpactUpdater' => static function (
