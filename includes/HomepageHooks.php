fd6bb6f97a1017d25f9ccf9e936a5f55941e1fda
Martin Urbanec <martin.urbanec@wikimedia.cz>
2026-02-11 17:49:56 +0100

    fix(SuggestedEdits): Fix inversed condition
    We want to short-circuit query features hook if Suggested edits
    are _disabled_, rather than enabled.
    
    This is fixing a train blocker. Regression testing to be added
    later.
    
    Bug: T417195
    Change-Id: Idc380719d98b1cb7b9a01c95d230b0452c90798f

--- includes/HomepageHooks.php
+++ includes/HomepageHooks.php
@@ -1301,7 +1301,7 @@ class HomepageHooks implements
 		$mwServices = MediaWikiServices::getInstance();
 		$growthServices = GrowthExperimentsServices::wrap( $mwServices );
 		// Avoid accessing Suggested Edits configuration when the feature is disabled (T369312)
-		if ( $growthServices->getFeatureManager()->isNewcomerTasksAvailable() ) {
+		if ( !$growthServices->getFeatureManager()->isNewcomerTasksAvailable() ) {
 			return;
 		}
 		$growthServices = GrowthExperimentsServices::wrap( $mwServices );
