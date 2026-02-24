f1b8e5ac1180b2807f05ffadb17aede68a862822
Martin Urbanec <martin.urbanec@wikimedia.cz>
2026-02-24 15:29:24 +0100

    tests: Introduce MentorRemoverTest
    We have no integration test that verifies various parts
    of mentor removing logic work as expected. This patch
    fixes that.
    
    Change-Id: I3240d1f85e515cf2ef97a113f7b343c77dc4160d

---
+++ tests/phpunit/integration/Mentorship/MentorRemoverTest.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace GrowthExperiments\Tests\Integration;
+
+use GrowthExperiments\GrowthExperimentsServices;
+use GrowthExperiments\Mentorship\ReassignMenteesJob;
+use GrowthExperiments\Mentorship\Store\MentorStore;
+use MediaWiki\Context\RequestContext;
+use MediaWiki\User\UserIdentity;
+use MediaWikiIntegrationTestCase;
+
+/**
+ * @group Database
+ * @covers \GrowthExperiments\Mentorship\MentorRemover
+ * @covers \GrowthExperiments\Mentorship\ReassignMenteesFactory
+ * @covers \GrowthExperiments\Mentorship\ReassignMentees
+ * @covers \GrowthExperiments\Mentorship\ReassignMenteesJob
+ */
+class MentorRemoverTest extends MediaWikiIntegrationTestCase {
+
+	protected function setUp(): void {
+		parent::setUp();
+
+		$this->setMainCache( CACHE_NONE );
+	}
+
+	private function getNewMentor(): UserIdentity {
+		$geServices = GrowthExperimentsServices::wrap( $this->getServiceContainer() );
+		$mentorProvider = $geServices->getMentorProvider();
+		$mentorWriter = $geServices->getMentorWriter();
+
+		$mentor = $this->getMutableTestUser()->getUserIdentity();
+		$mentorWriter->addMentor( $mentorProvider->newMentorFromUserIdentity( $mentor ), $mentor, '' );
+		$this->assertTrue( $mentorProvider->isMentor( $mentor ) );
+		return $mentor;
+	}
+
+	private function getMenteeForMentor( UserIdentity $mentor ) {
+		$geServices = GrowthExperimentsServices::wrap( $this->getServiceContainer() );
+		$mentorStore = $geServices->getMentorStore();
+
+		$mentee = $this->getMutableTestUser()->getUserIdentity();
+		$mentorStore->setMentorForUser( $mentee, $mentor, MentorStore::ROLE_PRIMARY );
+		$this->assertTrue( $mentorStore->hasAnyMentees( $mentor, MentorStore::ROLE_PRIMARY ) );
+		return $mentee;
+	}
+
+	public function testNoMentees() {
+		$geServices = GrowthExperimentsServices::wrap( $this->getServiceContainer() );
+		$mentorRemover = $geServices->getMentorRemover();
+		$mentorProvider = $geServices->getMentorProvider();
+
+		$mentor = $this->getNewMentor();
+
+		$status = $mentorRemover->removeMentor( $mentor, $mentor, '', RequestContext::getMain() );
+		$this->assertStatusOK( $status );
+		$this->assertFalse( $mentorProvider->isMentor( $mentor ) );
+	}
+
+	public function testWithMenteesOneMentor() {
+		$geServices = GrowthExperimentsServices::wrap( $this->getServiceContainer() );
+		$mentorRemover = $geServices->getMentorRemover();
+		$mentorProvider = $geServices->getMentorProvider();
+		$mentorStore = $geServices->getMentorStore();
+
+		$mentor = $this->getNewMentor();
+		$mentee = $this->getMenteeForMentor( $mentor );
+
+		$status = $mentorRemover->removeMentor( $mentor, $mentor, '', RequestContext::getMain() );
+		$this->assertStatusOK( $status );
+		$this->assertFalse( $mentorProvider->isMentor( $mentor ) );
+
+		$this->runJobs( runOptions: [ 'type' => ReassignMenteesJob::JOB_NAME ] );
+		$this->assertFalse( $mentorStore->hasAnyMentees( $mentor, MentorStore::ROLE_PRIMARY ) );
+		$this->assertNull( $mentorStore->loadMentorUser( $mentee, MentorStore::ROLE_PRIMARY ) );
+	}
+
+	public function testWithMenteesTwoMentors() {
+		$geServices = GrowthExperimentsServices::wrap( $this->getServiceContainer() );
+		$mentorRemover = $geServices->getMentorRemover();
+		$mentorProvider = $geServices->getMentorProvider();
+		$mentorStore = $geServices->getMentorStore();
+
+		$mentor = $this->getNewMentor();
+		$otherMentor = $this->getNewMentor();
+		$mentee = $this->getMenteeForMentor( $mentor );
+
+		$status = $mentorRemover->removeMentor( $mentor, $mentor, '', RequestContext::getMain() );
+		$this->assertStatusOK( $status );
+		$this->assertFalse( $mentorProvider->isMentor( $mentor ) );
+
+		$this->runJobs( runOptions: [ 'type' => ReassignMenteesJob::JOB_NAME ] );
+		$this->assertFalse( $mentorStore->hasAnyMentees( $mentor, MentorStore::ROLE_PRIMARY ) );
+		$this->assertTrue( $otherMentor->equals(
+			$mentorStore->loadMentorUser( $mentee, MentorStore::ROLE_PRIMARY )
+		) );
+	}
+
+	public function testWithMenteesOverLimit() {
+		$this->overrideConfigValue( 'GEMentorshipReassignMenteesBatchSize', 1 );
+
+		$geServices = GrowthExperimentsServices::wrap( $this->getServiceContainer() );
+		$mentorRemover = $geServices->getMentorRemover();
+		$mentorProvider = $geServices->getMentorProvider();
+		$mentorStore = $geServices->getMentorStore();
+
+		$mentor = $this->getNewMentor();
+		// FIXME: an alternate mentor should not be necessary, but it is for now
+		$otherMentor = $this->getNewMentor();
+
+		$mentee = $this->getMenteeForMentor( $mentor );
+		$menteeTwo = $this->getMenteeForMentor( $mentor );
+
+		$status = $mentorRemover->removeMentor( $mentor, $mentor, '', RequestContext::getMain() );
+		$this->assertStatusOK( $status );
+		$this->assertFalse( $mentorProvider->isMentor( $mentor ) );
+
+		$this->runJobs(
+			// NOTE: This is the key assert in this test (to verify the job continuation logic)
+			[ 'numJobs' => 2 ],
+			[ 'type' => ReassignMenteesJob::JOB_NAME ]
+		);
+		$this->assertFalse( $mentorStore->hasAnyMentees( $mentor, MentorStore::ROLE_PRIMARY ) );
+		$this->assertTrue( $otherMentor->equals(
+			$mentorStore->loadMentorUser( $mentee, MentorStore::ROLE_PRIMARY )
+		) );
+		$this->assertTrue( $otherMentor->equals(
+			$mentorStore->loadMentorUser( $menteeTwo, MentorStore::ROLE_PRIMARY )
+		) );
+	}
+}
