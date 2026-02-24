0c8e511e4d8b2b218e3569eb444cc9a71ce9cd91
Martin Urbanec <martin.urbanec@wikimedia.cz>
2026-02-24 14:54:09 +0100

    cleanup: Remove bunch of unnecessary code from ReassignMentees
    Change-Id: I87a1e85e22b62213bbab9a1ab17d39e3cd116592

--- includes/Mentorship/ReassignMenteesFactory.php
+++ includes/Mentorship/ReassignMenteesFactory.php
@@ -2,41 +2,23 @@
 
 namespace GrowthExperiments\Mentorship;
 
-use GrowthExperiments\Mentorship\Provider\MentorProvider;
 use GrowthExperiments\Mentorship\Store\MentorStore;
 use MediaWiki\Context\IContextSource;
 use MediaWiki\JobQueue\JobQueueGroupFactory;
-use MediaWiki\Language\FormatterFactory;
-use MediaWiki\Logger\LoggerFactory;
 use MediaWiki\User\UserIdentity;
+use Psr\Log\LoggerInterface;
 use Wikimedia\Rdbms\ILoadBalancer;
 
 class ReassignMenteesFactory {
 
-	private ILoadBalancer $dbLoadBalancer;
-	private IMentorManager $mentorManager;
-	private MentorProvider $mentorProvider;
-	private MentorStore $mentorStore;
-	private ChangeMentorFactory $changeMentorFactory;
-	private JobQueueGroupFactory $jobQueueGroupFactory;
-	private FormatterFactory $formatterFactory;
-
 	public function __construct(
-		ILoadBalancer $dbLoadBalancer,
-		IMentorManager $mentorManager,
-		MentorProvider $mentorProvider,
-		MentorStore $mentorStore,
-		ChangeMentorFactory $changeMentorFactory,
-		JobQueueGroupFactory $jobQueueGroupFactory,
-		FormatterFactory $formatterFactory
+		private LoggerInterface $logger,
+		private ILoadBalancer $dbLoadBalancer,
+		private IMentorManager $mentorManager,
+		private MentorStore $mentorStore,
+		private ChangeMentorFactory $changeMentorFactory,
+		private JobQueueGroupFactory $jobQueueGroupFactory
 	) {
-		$this->dbLoadBalancer = $dbLoadBalancer;
-		$this->mentorManager = $mentorManager;
-		$this->mentorProvider = $mentorProvider;
-		$this->mentorStore = $mentorStore;
-		$this->changeMentorFactory = $changeMentorFactory;
-		$this->jobQueueGroupFactory = $jobQueueGroupFactory;
-		$this->formatterFactory = $formatterFactory;
 	}
 
 	public function newReassignMentees(
@@ -45,18 +27,16 @@ class ReassignMenteesFactory {
 		IContextSource $context
 	): ReassignMentees {
 		$reassignMentees = new ReassignMentees(
+			$this->logger,
 			$this->dbLoadBalancer->getConnection( DB_PRIMARY ),
 			$this->mentorManager,
-			$this->mentorProvider,
 			$this->mentorStore,
 			$this->changeMentorFactory,
 			$this->jobQueueGroupFactory,
-			$this->formatterFactory->getStatusFormatter( $context ),
 			$performer,
 			$mentor,
 			$context
 		);
-		$reassignMentees->setLogger( LoggerFactory::getInstance( 'GrowthExperiments' ) );
 		return $reassignMentees;
 	}
 }
