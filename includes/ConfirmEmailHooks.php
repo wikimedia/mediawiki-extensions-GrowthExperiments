302a0091a52ced4cb39db1ade0c0730e9b290394
Martin Urbanec <martin.urbanec@wikimedia.cz>
2026-03-11 14:41:44 +0100

    cleanup: Remove emailability code
    Code related to Special:CreateAccount (adding
    a notice about the usefullness of adding an email)
    should be handled within the account creation experiment
    work.
    
    Bug: T411147
    Change-Id: I8e833c51b9d05006d9f5fbc87bd512f88f3413a2

--- includes/ConfirmEmailHooks.php
+++ includes/ConfirmEmailHooks.php
@@ -4,15 +4,11 @@ namespace GrowthExperiments;
 
 use MediaWiki\Auth\AuthManager;
 use MediaWiki\Context\RequestContext;
-use MediaWiki\Html\Html;
 use MediaWiki\MainConfigNames;
 use MediaWiki\MediaWikiServices;
-use MediaWiki\Message\Message;
 use MediaWiki\SpecialPage\Hook\AuthChangeFormFieldsHook;
-use MediaWiki\User\Hook\UserSendConfirmationMailHook;
-use MediaWiki\User\User;
 
-class ConfirmEmailHooks implements AuthChangeFormFieldsHook, UserSendConfirmationMailHook {
+class ConfirmEmailHooks implements AuthChangeFormFieldsHook {
 
 	/**
 	 * AuthChangeFormFields hook
@@ -44,52 +40,5 @@ class ConfirmEmailHooks implements AuthChangeFormFieldsHook, UserSendConfirmatio
 			$formDescriptor['email']['label-message'] = 'growthexperiments-confirmemail-emailrecommended';
 			$formDescriptor['email']['help-message'] = 'growthexperiments-confirmemail-emailhelp';
 		}
-	}
-
-	/**
-	 * Override confirmation email
-	 * @param User $user
-	 * @param array &$mail
-	 * @param array $info
-	 */
-	public function onUserSendConfirmationMail( $user, &$mail, $info ) {
-		$services = MediaWikiServices::getInstance();
-		$config = $services->getMainConfig();
-		if ( $config->get( MainConfigNames::UserEmailConfirmationUseHTML ) ) {
-			// Core's HTML version is in use, give it precedence
-			return;
-		}
-
-		$lang = RequestContext::getMain()->getLanguage();
-		// TODO different messages for different $info['type'] values?
-		$textParams = [
-			$info['ip'],
-			$user->getName(),
-			$info['confirmURL'],
-			$lang->userTimeAndDate( $info['expiration'], $user ),
-			$info['invalidateURL'],
-		];
-		$htmlParams = $textParams;
-		$htmlParams[2] = Message::rawParam( Html::element( 'a',
-			[
-				'href' => $info['confirmURL'],
-				'style' => 'background: #36C; color: white; padding: 0.45em 0.6em; font-weight: bold; ' .
-					'text-align: center; text-decoration: none;',
-			],
-			wfMessage( 'growthexperiments-confirmemail-confirm-button' )->text()
-		) );
-
-		$logo = $config->get( MainConfigNames::Logo );
-		$logoImage = Html::rawElement( 'p', [], Html::element(
-			'img', [ 'src' => $services->getUrlUtils()->expand( $logo, PROTO_CANONICAL ) ]
-		) );
-
-		$mail['subject'] = wfMessage( 'growthexperiments-confirmemail-confirm-subject' )->text();
-		$mail['body'] = [
-			'text' => wfMessage( 'growthexperiments-confirmemail-confirm-body-plaintext' )
-				->params( $textParams )->text(),
-			'html' => $logoImage . wfMessage( 'growthexperiments-confirmemail-confirm-body-html' )
-				->params( $htmlParams )->parse(),
-		];
 	}
 }
