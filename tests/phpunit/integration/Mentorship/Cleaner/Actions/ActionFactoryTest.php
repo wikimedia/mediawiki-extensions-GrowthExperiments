c88299593751ab778ad666ca586ea202aef1c137
Martin Urbanec <martin.urbanec@wikimedia.cz>
2026-07-07 17:27:20 +0100

    tests: Additional coverage for ActionFactoryTest
    Change-Id: I70cefdb5f0a279e3a03c04a0927a7503c6511291

--- tests/phpunit/integration/Mentorship/Cleaner/Actions/ActionFactoryTest.php
+++ tests/phpunit/integration/Mentorship/Cleaner/Actions/ActionFactoryTest.php
@@ -5,14 +5,18 @@ namespace GrowthExperiments\Tests\Integration;
 use GrowthExperiments\GrowthExperimentsServices;
 use GrowthExperiments\Mentorship\Cleaner\Actions\ActionFactory;
 use GrowthExperiments\Mentorship\Cleaner\Actions\IAction;
+use GrowthExperiments\Mentorship\Cleaner\Actions\MarkMentorAsAwayAction;
 use InvalidArgumentException;
+use MediaWiki\Extension\CommunityConfiguration\Tests\CommunityConfigurationTestHelpers;
 use MediaWikiIntegrationTestCase;
+use Wikimedia\TestingAccessWrapper;
 
 /**
  * @group Database
  * @covers \GrowthExperiments\Mentorship\Cleaner\Actions\ActionFactory
  */
 class ActionFactoryTest extends MediaWikiIntegrationTestCase {
+	use CommunityConfigurationTestHelpers;
 
 	private function getActionFactory(): ActionFactory {
 		return GrowthExperimentsServices::wrap( $this->getServiceContainer() )->getActionFactory();
@@ -47,5 +51,35 @@ class ActionFactoryTest extends MediaWikiIntegrationTestCase {
 	public function testNewFromClassNameRejectsUnknownClass() {
 		$this->expectException( InvalidArgumentException::class );
 		$this->getActionFactory()->newFromClassName( 'GrowthExperiments\\NotAnAction' );
+	}
+
+	/**
+	 * @dataProvider provideAwayDuration
+	 */
+	public function testAwayDuration( int $expected, array $config ) {
+		$this->overrideProviderConfig( $config, 'Mentorship' );
+
+		$actionFactory = $this->getActionFactory();
+		$action = $actionFactory->newFromClassName( MarkMentorAsAwayAction::class );
+		$this->assertInstanceOf( MarkMentorAsAwayAction::class, $action );
+		$this->assertSame(
+			$expected,
+			TestingAccessWrapper::newFromObject( $action )->awayDurationInDays
+		);
+	}
+
+	public static function provideAwayDuration() {
+		return [
+			'enabled, away after 30, removed after 90' => [ 61, [
+				'GEMentorshipShouldBeAutoremoved' => true,
+				'GEMentorshipAutoawayedAfterDays' => 30,
+				'GEMentorshipAutoremovedAfterDays' => 90,
+			] ],
+			'disabled, away after 30, removed after 90' => [ 30, [
+				'GEMentorshipShouldBeAutoremoved' => false,
+				'GEMentorshipAutoawayedAfterDays' => 30,
+				'GEMentorshipAutoremovedAfterDays' => 90,
+			] ],
+		];
 	}
 }
