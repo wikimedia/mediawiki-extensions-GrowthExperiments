01b5bcedbe16880fbd36e6fc830366dd3eee0841
Michael Große <mgrosse@wikimedia.org>
2026-07-16 18:11:58 +0200

    refactor(CreateAccount): cleanup username normalization
    The experiment was successful as is now being upstreamed to mediawiki
    core.
    The respective change in MediaWiki core is I438802e8a.
    
    The code populating `GECreateAccountExperimentV2` was already removed in
    I94ee1777d84c, this now just cleans up its usage.
    
    Bug: T430604
    Change-Id: I5371df39d582b230ff54050af82b7a0ac381489a

--- modules/ext.growthExperiments.Account/index.js
+++ modules/ext.growthExperiments.Account/index.js
@@ -3,10 +3,4 @@
 		const WelcomeSurvey = require( './WelcomeSurvey.js' );
 		WelcomeSurvey.setupLanguageSelector();
 	}
-	if ( mw.config.get( 'GECreateAccountExperimentV2' ) ) {
-		// T422295
-		const WE18ExperimentV1 = require( './WE18ExperimentV1.js' );
-		const enhancer = new WE18ExperimentV1();
-		enhancer.enhanceUsernameInputWithNameAdjustment();
-	}
 }() );
