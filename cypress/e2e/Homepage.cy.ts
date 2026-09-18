57094fe6824367b8aaca821b8827ea62bf797ff3
Michael Große <mgrosse@wikimedia.org>
2026-09-18 14:21:46 +0200

    test(AccountSetup): assert control experience is as expected
    This should hopefully catch regressions in the control experience, and
    be a starting point for validating the module order in the treatment
    experience.
    
    Bug: T432924
    Change-Id: I98eddd3d30ed95cd1908a7cb27bc082a74f4261d

--- cypress/e2e/Homepage.cy.ts
+++ cypress/e2e/Homepage.cy.ts
@@ -5,6 +5,56 @@ const homepage = new Homepage();
 const guidedTour = new GuidedTour();
 
 describe( 'Special:Homepage', () => {
+
+	it( 'shows the normal experience and modules for users not in an experiment', () => {
+		cy.viewport( 360, 780 );
+		cy.visit( 'index.php?title=JR-430_Mountaineer&mobileaction=toggle_view_mobile' );
+
+		cy.window().should( 'have.property', 'mw' );
+		cy.window().its( 'mw.tk' ).should( 'exist' );
+		cy.window().then(
+			async ( window: Cypress.AUTWindow & { mw: MediaWiki } ): Promise<void> => {
+				// @ts-expect-error tk definitions are still missing: wikimedia/typescript-types#69
+				window.mw.tk.clearExperimentOverrides();
+			},
+		);
+
+		// open user menu
+		cy.get( '#minerva-user-menu-checkbox' ).click();
+		// click sign-up
+		cy.get( '.minerva-user-menu a.menu__item--createaccount' ).should( 'be.visible' ).click();
+
+		const username = 'cypress-testuser-' + Math.random();
+		cy.get( '#wpName2' ).type( username );
+		const password = 'cypress-password' + Math.random();
+		cy.get( '#wpPassword2' ).type( password );
+		cy.get( '#wpRetype' ).type( password );
+		cy.get( '#wpCreateaccount' ).click();
+
+		cy.get( '#welcome-survey-form' ).should( 'be.visible' );
+		cy.get( '#welcome-survey-form button[name="save"]' ).scrollIntoView();
+		cy.get( '#welcome-survey-form button[name="save"]' ).click();
+		cy.contains( 'Go to your homepage' ).click();
+
+		cy.get( '.ext-growthExperiments-account-setup-step-1' ).should( 'not.exist' );
+		cy.get( '.homepage-welcome-notice' ).should( 'be.visible' );
+		cy.get( '.homepage-welcome-notice a' ).click();
+
+		cy.get( '.mw-ge-homepage-discovery-banner-mobile' ).should( 'be.visible' );
+		cy.get( '.mw-ge-homepage-discovery-banner-close' ).click();
+
+		cy.get( '.growthexperiments-homepage-module-mobile-summary' ).then( ( elements ) => {
+			const idsOfElementsOnHomepage = elements.toArray().map( ( element ) => element.id );
+			const expectedElementIds = [
+				'growthexperiments-homepage-module-startemail',
+				'growthexperiments-homepage-module-suggested-edits',
+				'growthexperiments-homepage-module-impact',
+				'growthexperiments-homepage-module-help',
+			];
+			expect( idsOfElementsOnHomepage ).to.deep.equal( expectedElementIds );
+		} );
+	} );
+
 	// T392940
 	it.skip( 'Shows a suggested edits card and allows navigation forwards and backwards through queue', () => {
 		cy.task( 'MwApi:CreateUser', { usernamePrefix: 'Alice' } ).then( ( { username, password }: {
